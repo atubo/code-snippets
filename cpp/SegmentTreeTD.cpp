ce6d99d741803551c88bfa5ce7163dbeaf8c019a
atubo <err722@yahoo.com>
2013-06-01 22:26:37 -0700

    Make SegmentTreeTD template class

--- cpp/SegmentTreeTD.cpp
+++ cpp/SegmentTreeTD.cpp
@@ -5,17 +5,10 @@ using namespace std;
 
 typedef int64_t ll;
 
-const ll MAXINT = numeric_limits<ll>::max();
-
-struct Node {
-	Node(): delta(0), val(MAXINT) {}
-	ll delta;
-	ll val;
-};
-
+template<class D>
 class SegmentTreeTD {
 public:
-	SegmentTreeTD(int N_, ll A[]): N(N_) {
+	SegmentTreeTD(int N_, D A[]): N(N_) {
 		int nlog = 0;
 		int n = N;
 		while (n > 0) {
@@ -23,7 +16,7 @@ public:
 			nlog++;
 		}
 		int msize = 2 * (1 << (nlog+1)) + 1;
-		M = new Node[msize];
+		M = new D[msize];
 		initialize(1, 0, N-1, A);
 	}
 
@@ -31,56 +24,67 @@ public:
 		delete[] M;
 	}
 
-	void initialize(int node, int b, int e, ll A[]) {
+	void initialize(int node, int b, int e, D A[]) {
 		if (b == e) {
-			M[node].val = A[b];
+			M[node] = A[b];
 		}
 		else {
 			initialize(2*node, b, (b+e)/2, A);
 			initialize(2*node+1, (b+e)/2+1, e, A);
-			M[node].val = min(M[2*node].val, M[2*node+1].val);
+			//M[node].val = min(M[2*node].val, M[2*node+1].val);
+            M[node].update(M[2*node], M[2*node+1]);
 		}
 	}
 
-	ll query(int i, int j) const {
-		return query(1, 0, N-1, i, j, 0);
+    template<class Q>
+	ll query(int i, int j, Q q) const {
+		return query(1, 0, N-1, i, j, q);
 	}
 
-	void update(int i, int j, ll d) {
-		update(1, 0, N-1, i, j, d);
+    template<class U>
+	void update(int i, int j, const U& u) {
+		update(1, 0, N-1, i, j, u);
 	}
 
 private:
 	int N;
-	Node* M;
+	D* M;
 
-	ll query(int node, int b, int e, int i, int j, ll acc) const {
+    template<class Q>
+	ll query(int node, int b, int e, int i, int j, Q q) const {
 		if (i > e || j < b) {
-			return MAXINT;
+			//return MAXINT;
+            return Q::nullValue();
 		}
 
-		acc += M[node].delta;
+		//acc += M[node].delta;
+        q.update(M[node]);
 		if (b >= i && e <= j) {
-			return M[node].val + acc;
+			//return M[node].val + acc;
+            return q.getValue(M[node]);
 		}
 
-		ll p1 = query(2*node, b, (b+e)/2, i, j, acc);
-		ll p2 = query(2*node+1, (b+e)/2+1, e, i, j, acc);
+		ll p1 = query(2*node, b, (b+e)/2, i, j, q);
+		ll p2 = query(2*node+1, (b+e)/2+1, e, i, j, q);
 
-		return min(p1, p2);
+		//return min(p1, p2);
+        return q.combineValues(p1, p2);
 	}
 
-	void update(int node, int b, int e, int i, int j, ll d) {
+    template<class U>
+	void update(int node, int b, int e, int i, int j, const U& u) {
 		if (i > e || j < b) {
 			return;
 		}
 		if (b >= i && e <= j) {
-			M[node].delta += d;
+			//M[node].delta += d;
+            u.updateNode(M[node]);
 			return;
 		}
-		update(2*node, b, (b+e)/2, i, j, d);
-		update(2*node+1, (b+e)/2+1, e, i, j, d);
-		M[node].val = min(M[2*node].delta + M[2*node].val,
-						  M[2*node+1].delta + M[2*node+1].val);
+		update(2*node, b, (b+e)/2, i, j, u);
+		update(2*node+1, (b+e)/2+1, e, i, j, u);
+		//M[node].val = min(M[2*node].delta + M[2*node].val,
+		//				  M[2*node+1].delta + M[2*node+1].val);
+        M[node].update(M[2*node], M[2*node+1]);
 	}
 };
--- cpp/SegmentTreeTDTest.cpp
+++ cpp/SegmentTreeTDTest.cpp
@@ -3,43 +3,97 @@
 
 CPPUNIT_TEST_SUITE_REGISTRATION(SegmentTreeTDTest);
 
+const ll MAXINT = numeric_limits<ll>::max();
+
+struct Node {
+	Node(): delta(0), val(MAXINT) {}
+    void update(const Node& left, const Node& right) {
+        val   = min(left.delta  + left.val,
+                    right.delta + right.val);
+    }
+	ll delta;
+	ll val;
+};
+
+struct Updator {
+    Updator(ll delta_): delta(delta_) {}
+    void updateNode(Node& node) const {
+        node.delta += delta;
+    }
+    ll delta;
+};
+
+struct Query {
+    Query(): delta(0) {}
+
+    static ll nullValue() {
+        return MAXINT;
+    }
+
+    void update(const Node& node) {
+        delta += node.delta;
+    }
+
+    ll getValue(const Node& node) const {
+        return node.val + delta;
+    }
+
+    ll combineValues(ll p1, ll p2) const {
+        return min(p1, p2);
+    }
+
+    ll delta;
+};
+
 void SegmentTreeTDTest::test1()
 {
-    int64_t a[] = {1, 2, 3, 4};
-    SegmentTreeTD tree(4, a);
-    tree.update(3, 3, -1);
-    tree.update(0, 0, -1);
-    int x = tree.query(0, 0);
+    //int64_t a[] = {1, 2, 3, 4};
+    Node a[4];
+    for (int i = 0; i < 4; i++) {
+        a[i].val = i+1;
+    }
+    SegmentTreeTD<Node> tree(4, a);
+    tree.update(3, 3, Updator(-1));
+    tree.update(0, 0, Updator(-1));
+    int x = tree.query(0, 0, Query());
     CPPUNIT_ASSERT_EQUAL(0, x);
 
-    x = tree.query(3, 3);
+    x = tree.query(3, 3, Query());
     CPPUNIT_ASSERT_EQUAL(3, x);
 
-    x = tree.query(0, 1);
+    x = tree.query(0, 1, Query());
     CPPUNIT_ASSERT_EQUAL(0, x);
 
-    x = tree.query(2, 3);
+    x = tree.query(2, 3, Query());
     CPPUNIT_ASSERT_EQUAL(3, x);
 }
 
 void SegmentTreeTDTest::test2()
 {
-    int64_t a[3] = {0};
-    SegmentTreeTD tree(3, a);
-    tree.update(0, 0, -1);
-    tree.update(0, 0, 1);
+    //int64_t a[3] = {0};
+    Node a[3];
     for (int i = 0; i < 3; i++) {
-        int x = tree.query(i, i);
+        a[i].val = 0;
+    }
+    SegmentTreeTD<Node> tree(3, a);
+    tree.update(0, 0, Updator(-1));
+    tree.update(0, 0, Updator(1));
+    for (int i = 0; i < 3; i++) {
+        int x = tree.query(i, i, Query());
         CPPUNIT_ASSERT_EQUAL(0, x);
     }
 }
 
 void SegmentTreeTDTest::test3()
 {
-    int64_t a[3] = {-2, -2, -2};
-    SegmentTreeTD tree(3, a);
-    tree.update(0, 2, 2);
-    tree.update(2, 2, -1);
-    int x = tree.query(0, 2);
+    //int64_t a[3] = {-2, -2, -2};
+    Node a[3];
+    for (int i = 0; i < 3; i++) {
+        a[i].val = -2;
+    }
+    SegmentTreeTD<Node> tree(3, a);
+    tree.update(0, 2, Updator(2));
+    tree.update(2, 2, Updator(-1));
+    int x = tree.query(0, 2, Query());
     CPPUNIT_ASSERT_EQUAL(-1, x);
 }
