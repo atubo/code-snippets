e9124abadb6306290f9fc9ba5a14131556aa4bd4
atubo <err722@yahoo.com>
2013-06-01 19:30:55 -0700

    Added more tests

--- cpp/SegmentTreeTDTest.cpp
+++ cpp/SegmentTreeTDTest.cpp
@@ -21,3 +21,25 @@ void SegmentTreeTDTest::test1()
     x = tree.query(2, 3);
     CPPUNIT_ASSERT_EQUAL(3, x);
 }
+
+void SegmentTreeTDTest::test2()
+{
+    int64_t a[3] = {0};
+    SegmentTreeTD tree(3, a);
+    tree.update(0, 0, -1);
+    tree.update(0, 0, 1);
+    for (int i = 0; i < 3; i++) {
+        int x = tree.query(i, i);
+        CPPUNIT_ASSERT_EQUAL(0, x);
+    }
+}
+
+void SegmentTreeTDTest::test3()
+{
+    int64_t a[3] = {-2, -2, -2};
+    SegmentTreeTD tree(3, a);
+    tree.update(0, 2, 2);
+    tree.update(2, 2, -1);
+    int x = tree.query(0, 2);
+    CPPUNIT_ASSERT_EQUAL(-1, x);
+}
--- cpp/SegmentTreeTDTest.h
+++ cpp/SegmentTreeTDTest.h
@@ -6,11 +6,15 @@
 class SegmentTreeTDTest:public CppUnit::TestFixture {
     CPPUNIT_TEST_SUITE(SegmentTreeTDTest);
     CPPUNIT_TEST(test1);
+    CPPUNIT_TEST(test2);
+    CPPUNIT_TEST(test3);
     CPPUNIT_TEST_SUITE_END();
 public:
     void setUp() {}
     void tearDown() {}
     void test1();
+    void test2();
+    void test3();
 };
 
 #endif
