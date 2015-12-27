7e34391e4b00bc6f07ae2ce426067669c1721e80
atubo <err722@yahoo.com>
2015-12-27 00:11:35 -0800

    Add Graph class

---
+++ cpp/Graph.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+
+// Undirected graph
+// Note graph node is 0-indexed
+class Graph {
+private:
+    const int N;
+    vector<vector<int> > adj;
+public:
+    Graph(int N_):N(N_) {
+        adj.resize(N);
+    }
+
+    // assume 0-indexed and no duplication
+    void addEdge(int i, int j) {
+        adj[i].push_back(j);
+        adj[j].push_back(i);
+    }
+
+    int size() const {return N;}
+
+    const vector<int>& getAdj(int i) const {
+        return adj[i];
+    }
+};
