0d0a31f7a8741e8e7e4f54a026f045de8240179a
atubo <err722@yahoo.com>
2016-11-25 09:58:53 -0800

    :sparkles: add ext_gcd and mod_solve

--- cpp/MathUtils.cpp
+++ cpp/MathUtils.cpp
@@ -1,4 +1,6 @@
 #include <cinttypes>
+#include <bits/stdc++.h>
+using namespace std;
 
 // 1 ^ 2 ^ 3 ^ ... ^ n
 int64_t xorUpto(int64_t n)
@@ -47,6 +49,27 @@ namespace Binom {
                 binom[i][j] = binom[i-1][j] + binom[i-1][j-1];
             }
         }
+    }
+}
+
+// (g, x, y) that a*x + b*y = g
+tuple<int64_t, int64_t, int64_t> ext_gcd(int64_t a, int64_t b) {
+    if (b == 0) {
+        return make_tuple(a, 1, 0);
+    }
+    int64_t dp, xp, yp;
+    tie(dp, xp, yp) = ext_gcd(b, a % b);
+    return make_tuple(dp, yp, xp - a / b * yp);
+}
+
+// find x that a*x = b mod n
+int64_t mod_solve(int64_t a, int64_t b, int n) {
+    int64_t d, xp, yp;
+    tie(d, xp, yp) = ext_gcd(a, n);
+    if (b % d == 0) {
+        return xp * (b / d) % n;
+    } else {
+        abort();
     }
 }
 
