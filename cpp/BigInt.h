9cc8de0acb84b983eef0a6c08e0794d90b629638
atubo <err722@yahoo.com>
2016-12-24 09:28:32 -0800

    fast BigInt

--- cpp/BigInt.h
+++ cpp/BigInt.h
@@ -1,5 +1,5 @@
 
-class BigInt {
+class BigIntStringRepr {
 public:
     static string mod(const string& A, const string& B) {
         vector<string> dbls;
@@ -65,5 +65,130 @@ public:
         while (p < N-1 && ret[p] == '0') p++;
         if (p != 0) ret = ret.substr(p);
         return ret;
+    }
+};
+
+class BigInt {
+    // split every 9 digits
+    // LSD in the first split, and so on
+    static const int SPLIT_LEN = 9;
+    static const int SPLIT_OVERFLOW = 1000000000;
+private:
+    vector<int> splits;
+
+public:
+    BigInt() {
+        splits.resize(1, 0);
+    }
+
+    BigInt(const string& s) {
+        const int n = s.length();
+        int imax = (n + SPLIT_LEN - 1) / SPLIT_LEN;
+        splits.resize(imax);
+        for (int i = 0; i < imax; i++) {
+            int start = max(0, n - (i+1)*SPLIT_LEN);
+            int end   = n - i*SPLIT_LEN;
+            splits[i] = stoi(s.substr(start, end-start));
+        }
+    }
+
+    string toString() const {
+        stringstream ss;
+        ss << splits.back();
+        for (int i = (int)splits.size()-2; i >= 0; i--) {
+            ss.width(SPLIT_LEN);
+            ss.fill('0');
+            ss << splits[i];
+        }
+        string ret = ss.str();
+        return ret;
+    }
+
+    friend BigInt operator % (const BigInt& a, const BigInt& b) {
+        vector<BigInt> dbls;
+        dbls.push_back(b);
+        while (dbls.back() < a) {
+            dbls.push_back(dbls.back() + dbls.back());
+        }
+
+        BigInt ret = a;
+        for (int i = (int)dbls.size()-1; i >= 0; i--) {
+            if (ret < dbls[i]) continue;
+            ret = ret - dbls[i];
+        }
+        return ret;
+    }
+
+    friend BigInt operator + (const BigInt& a, const BigInt& b) {
+        const int N = a.splits.size();
+        const int M = b.splits.size();
+
+        const int L = max(N, M);
+
+        BigInt ret;
+        ret.splits.resize(L);
+        
+        int carry = 0;
+        for (int i = 0; i < L; i++) {
+            int d = (i < N ? a.splits[i] : 0) + (i < M ? b.splits[i] : 0) + carry;
+            ret.splits[i] = d % SPLIT_OVERFLOW;
+            carry = (d >= SPLIT_OVERFLOW);
+        }
+
+        if (carry) {
+            ret.splits.push_back(1);
+        }
+
+        return ret;
+    }
+
+    friend BigInt operator - (const BigInt& a, const BigInt& b) {
+        // precondition: a >= b
+        assert(b <= a);
+
+        const int N = a.splits.size();
+        const int M = b.splits.size();
+
+        BigInt ret;
+        ret.splits.resize(N);
+        int borrow = 0;
+        for (int i = 0; i < N; i++) {
+            int d = a.splits[i] - (i < M ? b.splits[i] : 0) - borrow;
+            if (d < 0) {
+                d += SPLIT_OVERFLOW;
+                borrow = 1;
+            } else {
+                borrow = 0;
+            }
+            ret.splits[i] = d;
+        }
+        assert(borrow == 0);
+
+        for (int i = N-1; i > 0; i--) {
+            if (ret.splits[i] == 0) ret.splits.pop_back();
+            else break;
+        }
+
+        return ret;
+    }
+
+    friend bool operator <= (const BigInt& a, const BigInt& b) {
+        return (a < b || a == b);
+    }
+
+    friend bool operator < (const BigInt& a, const BigInt& b) {
+        if (a.splits.size() < b.splits.size()) return true;
+        if (a.splits.size() > b.splits.size()) return false;
+
+        for (int i = (int)a.splits.size()-1; i >= 0; i--) {
+            if (a.splits[i] < b.splits[i]) return true;
+            if (a.splits[i] > b.splits[i]) return false;
+        }
+
+        return false;
+    }
+
+    friend bool operator == (const BigInt& a, const BigInt& b) {
+        return a.splits == b.splits;
     }
 };
--- cpp/BigIntTest.cpp
+++ cpp/BigIntTest.cpp
@@ -5,33 +5,79 @@ using namespace std;
 
 #include <cppunit/extensions/HelperMacros.h>
 
-class BigIntTest:public CppUnit::TestFixture {
+class BigIntStringReprTest:public CppUnit::TestFixture {
 public:
     void setUp() {}
     void tearDown() {}
 
     void testSub() {
-        CPPUNIT_ASSERT(BigInt::sub("123", "45") == "78");
-        CPPUNIT_ASSERT(BigInt::sub("123", "123") == "0");
+        CPPUNIT_ASSERT(BigIntStringRepr::sub("123", "45") == "78");
+        CPPUNIT_ASSERT(BigIntStringRepr::sub("123", "123") == "0");
     }
 
     void testAdd() {
-        CPPUNIT_ASSERT(BigInt::add("123", "987") == "1110");
-        CPPUNIT_ASSERT(BigInt::add("999", "1") == "1000");
+        CPPUNIT_ASSERT(BigIntStringRepr::add("123", "987") == "1110");
+        CPPUNIT_ASSERT(BigIntStringRepr::add("999", "1") == "1000");
     }
 
     void testMod() {
-        CPPUNIT_ASSERT(BigInt::mod("12", "13") == "12");
-        CPPUNIT_ASSERT(BigInt::mod("13", "12") == "1");
-        CPPUNIT_ASSERT(BigInt::mod("24", "12") == "0");
-        CPPUNIT_ASSERT(BigInt::mod("100", "12") == "4");
+        CPPUNIT_ASSERT(BigIntStringRepr::mod("12", "13") == "12");
+        CPPUNIT_ASSERT(BigIntStringRepr::mod("13", "12") == "1");
+        CPPUNIT_ASSERT(BigIntStringRepr::mod("24", "12") == "0");
+        CPPUNIT_ASSERT(BigIntStringRepr::mod("100", "12") == "4");
     }
 
-    CPPUNIT_TEST_SUITE(BigIntTest);
+    CPPUNIT_TEST_SUITE(BigIntStringReprTest);
     CPPUNIT_TEST(testSub);
     CPPUNIT_TEST(testAdd);
     CPPUNIT_TEST(testMod);
     CPPUNIT_TEST_SUITE_END();
 };
 
+class BigIntTest:public CppUnit::TestFixture {
+public:
+    void setUp() {}
+    void tearDown() {}
+
+    void testSub() {
+        CPPUNIT_ASSERT(BigInt("123") - BigInt("45") == BigInt("78"));
+        CPPUNIT_ASSERT(BigInt("123") - BigInt("123") == BigInt("0"));
+        CPPUNIT_ASSERT(BigInt("987654321123456789") - BigInt("123456789987654321")
+                       == BigInt("864197531135802468"));
+        CPPUNIT_ASSERT(BigInt("987654321123456789") - BigInt("987654321123456789")
+                       == BigInt("0"));
+    }
+
+    void testAdd() {
+        CPPUNIT_ASSERT(BigInt("123") + BigInt("987") == BigInt("1110"));
+        CPPUNIT_ASSERT(BigInt("999999999") + BigInt("1") == BigInt("1000000000"));
+    }
+
+    void testMod() {
+        CPPUNIT_ASSERT(BigInt("12") % BigInt("13") == BigInt("12"));
+        CPPUNIT_ASSERT(BigInt("13") % BigInt("12") == BigInt("1"));
+        CPPUNIT_ASSERT(BigInt("24") % BigInt("12") == BigInt("0"));
+        CPPUNIT_ASSERT(BigInt("100") % BigInt("12") == BigInt("4"));
+        CPPUNIT_ASSERT(BigInt("77777777777777") % BigInt("1257386185646")
+                       == BigInt("1077220453371"));
+    }
+
+    void testToString() {
+        CPPUNIT_ASSERT(BigInt("0").toString() == "0");
+        CPPUNIT_ASSERT(BigInt("123").toString() == "123");
+        CPPUNIT_ASSERT(BigInt("12345678987654321").toString() == "12345678987654321");
+        CPPUNIT_ASSERT(BigInt("1234567000000000123456789").toString() ==
+                       "1234567000000000123456789");
+    }
+
+    CPPUNIT_TEST_SUITE(BigIntTest);
+    CPPUNIT_TEST(testSub);
+    CPPUNIT_TEST(testAdd);
+    CPPUNIT_TEST(testMod);
+    CPPUNIT_TEST(testToString);
+    CPPUNIT_TEST_SUITE_END();
+};
+
+
+CPPUNIT_TEST_SUITE_REGISTRATION(BigIntStringReprTest);
 CPPUNIT_TEST_SUITE_REGISTRATION(BigIntTest);
