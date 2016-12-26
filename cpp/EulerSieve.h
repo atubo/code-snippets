5b662171a1d11be52b6956143caab01a02ab85c6
atubo <err722@yahoo.com>
2016-12-26 00:18:42 -0800

    Euler sieve

---
+++ cpp/EulerSieve.h
@@ -0,0 +1,40 @@
+
+class EulerSieve {
+private:
+public:
+    vector<bool> isPrime;
+    vector<int> primes, miu, phi;
+
+    EulerSieve(int N) {
+        assert(N > 1);
+
+        isPrime.resize(N+1, true);
+        miu.resize(N+1);
+        phi.resize(N+1);
+
+        isPrime[0] = isPrime[1] = false;
+        miu[1] = 1;
+        phi[1] = 1;
+        for (int i = 2; i <= N; i++) {
+            if (isPrime[i]) {
+                primes.push_back(i);
+                miu[i] = -1;
+                phi[i] = i - 1;
+            }
+
+            for (int j = 0; j < (int)primes.size(); j++) {
+                int t = i * primes[j];
+                if ( t > N) break;
+                isPrime[t] = false;
+                if (i % primes[j] == 0) {
+                    miu[t] = 0;
+                    phi[t] = phi[i] * primes[j];
+                    break;
+                } else {
+                    miu[t] = -miu[i];
+                    phi[t] = phi[i] * (primes[j] - 1);
+                }
+            }
+        }
+    }
+};
---
+++ cpp/EulerSieveTest.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#include "EulerSieve.h"
+
+#include <cppunit/extensions/HelperMacros.h>
+
+#include <boost/assign/list_of.hpp>
+using namespace boost::assign;
+
+class EulerSieveTest:public CppUnit::TestFixture {
+public:
+    void setUp() {}
+    void tearDown() {}
+
+    void testSieve() {
+        EulerSieve sieve(20);
+        CPPUNIT_ASSERT(sieve.primes == list_of(2)(3)(5)(7)(11)(13)(17)(19));
+        //                                  0                5
+        CPPUNIT_ASSERT(sieve.phi == list_of(0)( 1)(1)( 2)(2)(4)(2)( 6)(4)(6)
+                                           (4)(10)(4)(12)(6)(8)(8)(16)(6)(18)(8));
+
+        //                                  0                5
+        CPPUNIT_ASSERT(sieve.miu == list_of(0)(1)(-1)(-1)(0)(-1)(1)(-1)(0)(0)
+                                           (1)(-1)(0)(-1)(1)(1)(0)(-1)(0)(-1)(0));
+    }
+
+    CPPUNIT_TEST_SUITE(EulerSieveTest);
+    CPPUNIT_TEST(testSieve);
+    CPPUNIT_TEST_SUITE_END();
+};
+
+CPPUNIT_TEST_SUITE_REGISTRATION(EulerSieveTest);
+
