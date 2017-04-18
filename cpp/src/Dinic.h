fca1b1ecb3779f9fed721e086170f5a4be6698dc
atubo <err722@yahoo.com>
2017-04-17 23:17:23 -0700

    当前弧优化

--- cpp/src/Dinic.h
+++ cpp/src/Dinic.h
@@ -4,7 +4,7 @@ private:
     static const int inf = 0x3f3f3f3f;
     static const int MAXN = 1000;
     static const int MAXM = 5 * MAXN * MAXN;
-    int head[MAXN];
+    int head[MAXN], curr[MAXN];
     struct Edge {
         int to, next, cap;
     } E[MAXM];
@@ -12,23 +12,27 @@ private:
 
 public:
     Dinic() {
+        reset();
+    }
+
+    void reset() {
+        e = 0;
         memset(head, -1, sizeof(head));
     }
 
-    void addEdge(int x, int y, int z) {
-        E[e] = {y, head[x], z};
+    void addEdge(int x, int y, int w, int rw = 0) {
+        E[e] = {y, head[x], w};
         head[x] = e++;
-        E[e] = {x, head[y], 0};
+        E[e] = {x, head[y], rw};
         head[y] = e++;
     }
 
 private:
     int d[MAXN];
 
-
     bool bfs(int s, int t) {
-        queue<int> q;
         memset(d, -1, sizeof(d));
+        queue<int> q;
         q.push(t);
         d[t] = 0;
         while (!q.empty()) {
@@ -39,16 +43,17 @@ private:
                 if (d[v] == -1 && E[i^1].cap) {
                     d[v] = d[u] + 1;
                     q.push(v);
+                    if (v == s) return true;
                 }
             }
         }
-        return d[s] != -1;
+        return false;
     }
 
     int dfs(int x, int low, int t) {
         if (x == t || !low) return low;
         int ret = 0;
-        for (int i = head[x]; i != -1; i = E[i].next) {
+        for (int &i = curr[x]; i != -1; i = E[i].next) {
             int v = E[i].to;
             if (d[v] == d[x] - 1) {
                 int k = dfs(v, min(low-ret, E[i].cap), t);
@@ -66,6 +71,9 @@ public:
     int dinic(int s, int t) {
         int ans = 0;
         while (bfs(s, t)) {
+            for (int i = 0; i < MAXN; i++) {
+                curr[i] = head[i];
+            }
             int k = dfs(s, inf, t);
             if (k > 0) ans += k;
         }
