79dcec6278f3682aed179598af3db678da4a444b
atubo <err722@yahoo.com>
2017-04-17 20:46:15 -0700

    Dinic refactoring

--- cpp/src/Dinic.h
+++ cpp/src/Dinic.h
@@ -1,70 +1,74 @@
 
-namespace Dinic {
-const int inf = 0x3f3f3f3f;
-const int MAXN = 1000;
-const int MAXM = 5 * MAXN * MAXN;
-int head[MAXN];
-struct Edge {
-    int to, next, cap;
-} E[MAXM];
-int e;
+class Dinic {
+private:
+    static const int inf = 0x3f3f3f3f;
+    static const int MAXN = 1000;
+    static const int MAXM = 5 * MAXN * MAXN;
+    int head[MAXN];
+    struct Edge {
+        int to, next, cap;
+    } E[MAXM];
+    int e;
 
-void init() {
-    memset(head, -1, sizeof(head));
-}
+public:
+    Dinic() {
+        memset(head, -1, sizeof(head));
+    }
 
-void addEdge(int x, int y, int z) {
-    E[e] = {y, head[x], z};
-    head[x] = e++;
-    E[e] = {x, head[y], 0};
-    head[y] = e++;
-}
+    void addEdge(int x, int y, int z) {
+        E[e] = {y, head[x], z};
+        head[x] = e++;
+        E[e] = {x, head[y], 0};
+        head[y] = e++;
+    }
 
-int d[MAXN];
+private:
+    int d[MAXN];
 
 
-bool bfs(int s, int t) {
-    queue<int> q;
-    memset(d, -1, sizeof(d));
-    q.push(t);
-    d[t] = 0;
-    while (!q.empty()) {
-        int u = q.front();
-        q.pop();
-        for (int i = head[u]; i != -1; i = E[i].next) {
+    bool bfs(int s, int t) {
+        queue<int> q;
+        memset(d, -1, sizeof(d));
+        q.push(t);
+        d[t] = 0;
+        while (!q.empty()) {
+            int u = q.front();
+            q.pop();
+            for (int i = head[u]; i != -1; i = E[i].next) {
+                int v = E[i].to;
+                if (d[v] == -1 && E[i^1].cap) {
+                    d[v] = d[u] + 1;
+                    q.push(v);
+                }
+            }
+        }
+        return d[s] != -1;
+    }
+
+    int dfs(int x, int low, int t) {
+        if (x == t || !low) return low;
+        int ret = 0;
+        for (int i = head[x]; i != -1; i = E[i].next) {
             int v = E[i].to;
-            if (d[v] == -1 && E[i^1].cap) {
-                d[v] = d[u] + 1;
-                q.push(v);
+            if (d[v] == d[x] - 1) {
+                int k = dfs(v, min(low-ret, E[i].cap), t);
+                if (k > 0) {
+                    E[i].cap -= k;
+                    E[i^1].cap += k;
+                    ret += k;
+                }
             }
         }
+        return ret;
     }
-    return d[s] != -1;
-}
 
-int dfs(int x, int low, int t) {
-    if (x == t || !low) return low;
-    int ret = 0;
-    for (int i = head[x]; i != -1; i = E[i].next) {
-        int v = E[i].to;
-        if (d[v] == d[x] - 1) {
-            int k = dfs(v, min(low-ret, E[i].cap), t);
-            if (k > 0) {
-                E[i].cap -= k;
-                E[i^1].cap += k;
-                ret += k;
-            }
+public:
+    int dinic(int s, int t) {
+        int ans = 0;
+        while (bfs(s, t)) {
+            int k = dfs(s, inf, t);
+            if (k > 0) ans += k;
         }
+        return ans;
     }
-    return ret;
-}
-
-int dinic(int s, int t) {
-    int ans = 0;
-    while (bfs(s, t)) {
-        int k = dfs(s, inf, t);
-        if (k > 0) ans += k;
-    }
-    return ans;
-}
-}
+};
--- cpp/test/DinicTest.cpp
+++ cpp/test/DinicTest.cpp
@@ -12,20 +12,20 @@ public:
     void tearDown() {}
 
     void testFlowCalc() {
-        Dinic::init();
+        Dinic dinic;
 
-        Dinic::addEdge(0, 1, 8);
-        Dinic::addEdge(0, 2, 4);
-        Dinic::addEdge(2, 1, 4);
-        Dinic::addEdge(1, 3, 2);
-        Dinic::addEdge(1, 4, 2);
-        Dinic::addEdge(2, 3, 1);
-        Dinic::addEdge(2, 4, 4);
-        Dinic::addEdge(3, 4, 6);
-        Dinic::addEdge(3, 5, 9);
-        Dinic::addEdge(4, 5, 7);
+        dinic.addEdge(0, 1, 8);
+        dinic.addEdge(0, 2, 4);
+        dinic.addEdge(2, 1, 4);
+        dinic.addEdge(1, 3, 2);
+        dinic.addEdge(1, 4, 2);
+        dinic.addEdge(2, 3, 1);
+        dinic.addEdge(2, 4, 4);
+        dinic.addEdge(3, 4, 6);
+        dinic.addEdge(3, 5, 9);
+        dinic.addEdge(4, 5, 7);
 
-        CPPUNIT_ASSERT_EQUAL(8, Dinic::dinic(0, 5));
+        CPPUNIT_ASSERT_EQUAL(8, dinic.dinic(0, 5));
     }
 
     CPPUNIT_TEST_SUITE(DinicTest);
