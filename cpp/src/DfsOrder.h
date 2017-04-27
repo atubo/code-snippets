481ba9644b2fd10178b0fffcdf87b6b411c3d8bb
atubo <err722@yahoo.com>
2017-04-26 22:52:43 -0700

    code to do DFS order of a tree

---
+++ cpp/src/DfsOrder.h
@@ -0,0 +1,25 @@
+// generate DFS order for a tree
+#include "Graph.cpp"
+
+class DfsOrder {
+public:
+    static const int MAXN = 100010;
+    vector<int> A;
+    int first[MAXN], last[MAXN];
+    int order;
+
+    DfsOrder() {
+        order = 0;
+    }
+
+    void dfs(const Graph &g, int u, int f) {
+        first[u] = order++;
+        A.push_back(u);
+        for (int eidx = g.head[u]; eidx != -1; eidx = g.E[eidx].next) {
+            int v = g.E[eidx].to;
+            if (v != f) dfs(g, v, u);
+        }
+        last[u] = order++;
+        A.push_back(u);
+    }
+};
---
+++ cpp/test/DfsOrderTest.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#include "DfsOrder.h"
+
+#include <boost/assign/list_of.hpp>
+
+#include <cppunit/extensions/HelperMacros.h>
+
+using namespace boost::assign;
+
+class DfsOrderTest : public CppUnit::TestFixture {
+private:
+    void addEdge(Graph &g, int u, int v) {
+        g.addEdge(u, v);
+        g.addEdge(v, u);
+    }
+public:
+    void setUp() {}
+    void tearDown() {}
+
+    void testDfn() {
+        Graph g(6);
+        addEdge(g, 0, 1);
+        addEdge(g, 0, 2);
+        addEdge(g, 0, 3);
+        addEdge(g, 2, 4);
+        addEdge(g, 2, 5);
+
+        DfsOrder dfn;
+        dfn.dfs(g, 0, -1);
+        CPPUNIT_ASSERT(dfn.A == list_of(0)(3)(3)(2)(5)(5)(4)(4)(2)(1)(1)(0));
+        vector<int> first, last;
+        for (int i = 0; i < 6; i++) {
+            first.push_back(dfn.first[i]);
+            last.push_back(dfn.last[i]);
+        }
+        CPPUNIT_ASSERT(first == list_of(0)(9)(3)(1)(6)(4));
+        CPPUNIT_ASSERT(last  == list_of(11)(10)(8)(2)(7)(5));
+    }
+
+    CPPUNIT_TEST_SUITE(DfsOrderTest);
+    CPPUNIT_TEST(testDfn);
+    CPPUNIT_TEST_SUITE_END();
+};
+
+CPPUNIT_TEST_SUITE_REGISTRATION(DfsOrderTest);
