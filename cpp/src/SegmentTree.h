050a595b0b35a67d3bf8799e17b369d517a5debf
atubo <err722@yahoo.com>
2018-01-04 07:30:35 -0800

    Simple implementation of segment tree

---
+++ cpp/src/SegmentTree.h
@@ -0,0 +1,61 @@
+const int MAXN = 50010 * 2;
+
+int64_t val[131100], lazy[131100];
+
+class SegmentTree {
+    int N;
+    //vector<int64_t> val, lazy;
+
+public:
+    SegmentTree(int N_): N(N_) {}
+    // add t to range [a, b]
+    void update(int t, int a, int b) {
+        update(1, t, 1, N, a, b);
+    }
+
+    // query range sum in [a, b]
+    int64_t query(int a, int b) {
+        return query(1, a, b, 1, N);
+    }
+
+private:
+    // add t to range [a, b], current node range is [l, r]
+    void update(int k, int t, int l, int r, int a, int b) {
+        if (a > b) return;
+        pushDown(k, l, r);
+        if (a <= l && r <= b) {
+            val[k] += t * (r-l+1);
+            lazy[k] += t;
+            return;
+        }
+        if (l == r) return;
+        int mid = (l + r) / 2;
+        if (a <= mid) update(2*k,   t, l, mid, a, b);
+        if (mid < b)  update(2*k+1, t, mid+1, r, a, b);
+        val[k] = val[2*k] + val[2*k+1];
+    }
+
+    void pushDown(int k, int l, int r) {
+        if (!lazy[k] || l == r) return;
+        lazy[2*k] += lazy[k];
+        lazy[2*k+1] += lazy[k];
+        int mid = (l + r) / 2;
+        val[2*k] += (mid - l + 1) * lazy[k];
+        val[2*k+1] += (r - mid) * lazy[k];
+        lazy[k] = 0;
+    }
+
+    // query range sum in [a, b], current node is [L, R]
+    int64_t query(int k, int a, int b, int L, int R) {
+        if (!k) return 0;
+        if (b < L || a > R) return 0;
+        pushDown(k, L, R);
+        if (a <= L && R <= b) return val[k];
+        int64_t sum = 0;
+        int mid = (L + R) / 2;
+        if (a <= mid) sum += query(2*k, a, b, L, mid);
+        if (mid < b)  sum += query(2*k+1, a, b, mid+1, R);
+        return sum;
+    }
+};
+
