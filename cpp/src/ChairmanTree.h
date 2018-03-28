c03c9cd8fb104b89e1776e90202ed41ab408ba23
atubo <err722@yahoo.com>
2018-03-28 07:04:02 -0700

    update ChairmenTree

--- cpp/src/ChairmanTree.h
+++ cpp/src/ChairmanTree.h
@@ -1,6 +1,5 @@
-const int MAXN = 500010;
-
-struct ChairmanTree {
+class PersistentSegmentTree {
+public:
     struct Node {
         // L and R are indices of its children
         // real left and right boundaries are not kept in the node
@@ -12,14 +11,15 @@ struct ChairmanTree {
     };
 
     int N; // number of positions, index is 1-based
-    int M; // number of weights, index is 1-based
-    int T_cnt;
+    int M; // number of weights, index is 0-based
+    int *root;
+    Node *T;
+    int T_cnt = 1;
 
-    // use static to avoid dynamic memory management
-    static int root[MAXN];
-    static Node T[MAXN*21];
-
-    ChairmanTree(int N_, int M_): N(N_), M(M_), T_cnt(1) {}
+    PersistentSegmentTree(int N_, int M_): N(N_), M(M_) {
+        root = new int[N+1]{};
+        T = new Node[(N+1) * int(log2(M)+1)]{};
+    }
 
     // insert a new weight node x with value t
     // current range is [l, r]
@@ -36,10 +36,21 @@ struct ChairmanTree {
     // for user's convenience
     void insert(int &now, int from, int x, int t) {
         now = from;
-        insert(now, x, t, 1, M);
+        insert(now, x, t, 0, M-1);
+    }
+
+    // query in range [start, end]
+    int query(int rootIndex, int start, int end) {
+        return query(root[rootIndex], start, end, 0, M-1);
+    }
+
+    int query(int node, int start, int end, int l, int r) {
+        if (l > r) return 0;
+        if (start <= l && r <= end) return T[node].sum;
+        int mid = (l + r) / 2;
+        int ret = 0;
+        if (start <= mid) ret += query(T[node].L, start, end, l, mid);
+        if (mid+1 <= end) ret += query(T[node].R, start, end, mid+1, r);
+        return ret;
     }
 };
-
-using Node = ChairmanTree::Node;
-int ChairmanTree::root[MAXN];
-Node ChairmanTree::T[MAXN*21];
