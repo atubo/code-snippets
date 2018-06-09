c19e3fba91b2f38a95e95d3b3867fffa8da23bce
atubo <err722@yahoo.com>
2018-06-09 16:21:07 -0700

    [FFT] initial version

---
+++ cpp/src/fft.h
@@ -0,0 +1,50 @@
+
+class Fft {
+public:
+    constexpr const static double PI = 3.14159265359;
+    static void four1(vector<double> &data, const int isign) {
+        int nn = data.size() / 2;
+        int n = nn << 1;
+        int j = 1;
+        for (int i = 1; i < n; i+=2) {
+            if (j > i) {
+                swap(data[j-1], data[i-1]);
+                swap(data[j], data[i]);
+            }
+            int m = nn;
+            while (m >= 2 && j > m) {
+                j -= m;
+                m >>= 1;
+            }
+            j += m;
+        }
+
+        int mmax = 2;
+        while (n > mmax) {
+            int istep = mmax << 1;
+            double theta = isign * (2 * PI / mmax);
+            double wtemp = sin(0.5 * theta);
+            double wpr = - 2.0 * wtemp * wtemp;
+            double wpi = sin(theta);
+            double wr = 1.0;
+            double wi = 0.0;
+            for (int m = 1; m < mmax; m += 2) {
+                for (int i = m; i <= n; i += istep) {
+                    j = i + mmax;
+                    double tempr = wr * data[j-1] - wi * data[j];
+                    double tempi = wr * data[j] + wi * data[j-1];
+                    data[j-1] = data[i-1] - tempr;
+                    data[j] = data[i] - tempi;
+                    data[i-1] += tempr;
+                    data[i] += tempi;
+                }
+                wr = (wtemp=wr)*wpr - wi*wpi + wr;
+                wi = wi*wpr + wtemp*wpi + wi;
+            }
+            mmax = istep;
+        }
+    }
+};
+
+
+
---
+++ cpp/test/fft_test.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#include "fft.h"
+
+#include <cppunit/extensions/HelperMacros.h>
+
+class FftTest:public CppUnit::TestFixture {
+public:
+    void setUp() {}
+    void tearDown() {}
+
+    void testLength4() {
+        // FFT of [1, 2, 3, 4]
+        vector<double> data(8);
+        for (int i = 0; i < 4; i++) data[2*i] = i+1;
+        Fft::four1(data, 1);
+        vector<double> expRe = {10, -2, -2, -2};
+        vector<double> expIm = {0, -2, 0, 2};
+        for (int i = 0; i < 4; i++) {
+            CPPUNIT_ASSERT_DOUBLES_EQUAL(data[2*i], expRe[i], 1e-6);
+            CPPUNIT_ASSERT_DOUBLES_EQUAL(data[2*i+1], expIm[i], 1e-6);
+        }
+    }
+
+    void testPolynomicalMultiplication() {
+        // (1+2x+3x^2+4x^3)(5+6x+7x^2+8x^3)
+        // answser should be 32x^6+52x^5+61x^4+60x^3+34x^2+16x+5
+        vector<double> a(16), b(16);
+        for (int i = 0; i < 4; i++) {
+            a[2*i] = i+1;
+            b[2*i] = i+5;
+        }
+        Fft::four1(a, 1);
+        Fft::four1(b, 1);
+
+        vector<double> c(16);
+        for (int i = 0; i < 8; i++) {
+            double x1 = a[2*i], y1 = a[2*i+1];
+            double x2 = b[2*i], y2 = b[2*i+1];
+            c[2*i] = x1*x2 - y1*y2;
+            c[2*i+1] = x1*y2 + y1*x2;
+        }
+        Fft::four1(c, -1);
+        vector<double> expected = {5, 16, 34, 60, 61, 52, 32, 0};
+        for (int i = 0; i < 8; i++) {
+            CPPUNIT_ASSERT_DOUBLES_EQUAL(expected[i], c[2*i]/8, 1e-6);
+            CPPUNIT_ASSERT_DOUBLES_EQUAL(0, c[2*i+1], 1e-6);
+        }
+    }
+
+    CPPUNIT_TEST_SUITE(FftTest);
+    CPPUNIT_TEST(testLength4);
+    CPPUNIT_TEST(testPolynomicalMultiplication);
+    CPPUNIT_TEST_SUITE_END();
+};
+
+CPPUNIT_TEST_SUITE_REGISTRATION(FftTest);
