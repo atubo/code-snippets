43435b597c855ec4ac1492c25bfa3ae186794c50
atubo <err722@yahoo.com>
2018-08-11 12:24:46 -0700

    [matrix] refactoring

--- cpp/src/math/matrix.h
+++ cpp/src/math/matrix.h
@@ -25,47 +25,6 @@ public:
         delete[] m_;
     }
 
-    void alloc() {
-        m_ = new int*[n_]{};
-        for (int i = 0; i < n_; i++) {
-            m_[i] = new int[n_]{};
-        }
-    }
-
-
-#if 0
-    Matrix& operator *= (const Matrix &rhs) {
-        Matrix ret(n_);
-        for (int i = 0; i < n_; i++) {
-            for (int j = 0; j < n_; j++) {
-                for (int k = 0; k < n_; k++) {
-                    ret.m_[i][j] += (m_[i][k] * rhs.m_[k][j]) % MOD;
-                    ret.m_[i][j] %= MOD;
-                }
-            }
-        }
-        swap(ret);
-        return *this;
-    }
-#endif
-
-    void mul(const Matrix &rhs, Matrix &tmp) {
-        for (int i = 0; i < n_; i++) {
-            memset(tmp.m_[i], 0, n_ * sizeof(int));
-            for (int k = 0; k < n_; k++) {
-                if (!m_[i][k]) continue;
-                for (int j = 0; j < n_; j++) {
-                    tmp.m_[i][j] += m_[i][k] * rhs.m_[k][j];
-                }
-            }
-        }
-        swap(tmp);
-    }
-
-    void swap(Matrix &other) {
-        std::swap(m_, other.m_);
-    }
-
     static Matrix power(Matrix x, int n) {
         Matrix ret(x.n_), tmp(x.n_);
         for (int i = 0; i < x.n_; i++) ret.m_[i][i] = 1;
@@ -84,5 +43,30 @@ public:
             }
             printf("\n");
         }
+    }
+
+private:
+    void alloc() {
+        m_ = new int*[n_]{};
+        for (int i = 0; i < n_; i++) {
+            m_[i] = new int[n_]{};
+        }
+    }
+
+    void mul(const Matrix &rhs, Matrix &tmp) {
+        for (int i = 0; i < n_; i++) {
+            memset(tmp.m_[i], 0, n_ * sizeof(int));
+            for (int k = 0; k < n_; k++) {
+                if (!m_[i][k]) continue;
+                for (int j = 0; j < n_; j++) {
+                    tmp.m_[i][j] += m_[i][k] * rhs.m_[k][j];
+                }
+            }
+        }
+        swap(tmp);
+    }
+
+    void swap(Matrix &other) {
+        std::swap(m_, other.m_);
     }
 };
