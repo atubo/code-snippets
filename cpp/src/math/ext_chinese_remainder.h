d97b82090545aa88b2d8fdefa838a6278f970c4e
atubo <err722@yahoo.com>
2018-08-18 22:45:17 -0700

    [ext Chinese remainder] fix for overflow

--- cpp/src/math/ext_chinese_remainder.h
+++ cpp/src/math/ext_chinese_remainder.h
@@ -1,24 +1,20 @@
 class ExtChineseRemainder {
 private:
-    bool started_ = false;
-    int64_t b_, m_;
+    int64_t b_ = 0, m_ = 1;
 public:
     ExtChineseRemainder() {}
 
     bool add(int64_t b2, int64_t m2) {
-        if (!started_) {
-            b_ = b2;
-            m_ = m2;
-            started_ = true;
-            return true;
-        }
-
         int64_t g, u, v;
         ModularUtils::ext_gcd(m_, m2, g, u, v);
         if (b_ % g != b2 % g) return false;
-        b_ = m2 / g * b_ *v + m_ / g * b2 * u;
-        m_ = m2 / g * m_;
-        b_ = (b_ % m_ + m_) % m_;
+        int64_t lcm = m2 / g * m_;
+        int64_t t1 = ModularUtils::mul_long(m2/g, b_, lcm);
+        t1 = ModularUtils::mul_long(t1, v, lcm);
+        int64_t t2 = ModularUtils::mul_long(m_/g, b2, lcm);
+        t2 = ModularUtils::mul_long(t2, u, lcm);
+        b_ = ((t1 + t2) % lcm + lcm) % lcm;
+        m_ = lcm;
         return true;
     }
 
