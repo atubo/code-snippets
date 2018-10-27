54bf5284841282893d1da0d94853a02aadb2aab5
atubo <err722@yahoo.com>
2018-10-27 15:41:11 -0700

    [ImplicitTreap] new implementation

--- cpp/src/data_structures/implicit_treap.h
+++ cpp/src/data_structures/implicit_treap.h
@@ -136,3 +136,210 @@ namespace ImplicitTreap {
     }
 };
 
+
+// 1-indexed, 0 means null
+class ImplicitTreap2 {
+    struct Node {
+        int prior, size;
+        int val;  // value stored in the array
+        int sum;  // whatever info you want to maintain in segtree for the node
+        int lazy; // whatever lazy update you want to do
+        int l, r;
+    };
+public:
+    ImplicitTreap2(int cap): cap_(cap) {
+        alloc();
+    }
+
+    ~ImplicitTreap2() {
+        dealloc();
+    }
+
+    /**
+     * Note a should also be 1-indexed
+     * @param a: the array based on which the treap is built
+     * @return: root index
+     */
+    int build(const vector<int>& a) {
+        root_ = build(a, 1, a.size());
+        return root_;
+    }
+
+    int rangeQuery(int l, int r) { // [l,r]
+        int t = root_;
+        int L, mid, R;
+        split(t, L, mid, l-1);
+        split(mid, t, R, r-l+1);
+        int ans = nodes_[t].sum;
+        merge(mid, L, t);
+        merge(root_, mid, R);
+        return ans;
+    }
+
+    void rangeUpdate(int l, int r, int val) { // [l,r]
+        int t = root_;
+        int L, mid, R;
+        split(t, L, mid, l-1);
+        split(mid, t, R, r-l+1);
+        nodes_[t].lazy += val;
+        merge(mid, L, t);
+        merge(root_, mid, R);
+    }
+
+    int operator[](int n) const {
+        assert(1 <= n && n <= sz(root_));
+        int t = root_;
+        while (true) {
+            int left_cnt = sz(nodes_[t].l);
+            if (left_cnt == n-1) return nodes_[t].val;
+            else if (left_cnt < n-1) {
+                t = nodes_[t].r;
+                n -= left_cnt + 1;
+            } else {
+                t = nodes_[t].l;
+            }
+        }
+        assert(false);
+        return -1;
+    }
+
+    /**
+     * insert val after position pos, if pos = 0, insert at the begining
+     * @param pos: position
+     * @param val: value
+     */
+    void insert(int pos, int val) {
+        assert(0 <= pos && pos <= sz(root_));
+        int newnode = allocNode(val);
+        if (pos == 0) {
+            merge(root_, newnode, root_);
+        } else if (pos == sz(root_)) {
+            merge(root_, root_, newnode);
+        } else {
+            int t = root_;
+            int L, R;
+            split(t, L, R, pos);
+            merge(t, L, newnode);
+            merge(root_, t, R);
+        }
+    }
+
+private:
+    int cap_;
+    int curr_ = 1;
+    int root_;
+    Node *nodes_;
+
+    void alloc() {
+        nodes_ = new Node[cap_+1]{};
+    }
+
+    void dealloc() {
+        delete[] nodes_;
+    }
+
+    int sz(int t) const {
+        return t ? nodes_[t].size : 0;
+    }
+
+    void reset(int t) {
+        // no need to reset lazy coz when we call this lazy would
+        // itself be propagated
+        if (t) nodes_[t].sum = nodes_[t].val;
+    }
+
+    // combining two ranges of segtree
+    void combine(int& t, int l, int r) {
+        // TODO: can we remove this check?
+        if (!l || !r) {
+            t = (l ? l : r);
+            return;
+        }
+        nodes_[t].sum = nodes_[l].sum + nodes_[r].sum;
+    }
+
+    void pushUp(int t) {
+        if (!t) return;
+        Node &node = nodes_[t];
+        node.size = sz(node.l) + 1 + sz(node.r);
+        // reset the value of current node assuming it now represents
+        // a single element of the array
+        reset(t);
+        // propaget lazy before combining
+        pushDown(node.l);
+        pushDown(node.r);
+        combine(t, node.l, t);
+        combine(t, t, node.r);
+    }
+
+    // invariant: values are right after lazy is pushed down
+    void pushDown(int t) {
+        if (!t || !nodes_[t].lazy) return;
+        Node &node = nodes_[t];
+        // operation of lazy
+        node.val += node.lazy;
+        node.sum += node.lazy * sz(t);
+        // propagate lazy
+        if (node.l) nodes_[node.l].lazy += node.lazy;
+        if (node.r) nodes_[node.r].lazy += node.lazy;
+        node.lazy = 0;
+    }
+
+
+    // l -> leftarray, r->rightarray
+    void merge(int& t, int l, int r) {
+        pushDown(l);
+        pushDown(r);
+        Node &nl = nodes_[l];
+        Node &nr = nodes_[r];
+        if (!l || !r) t = l ? l : r;
+        else if (nl.prior > nr.prior) merge(nl.r, nl.r, r), t=l;
+        else merge(nr.l, l, nr.l), t=r;
+        pushUp(t);
+    }
+
+    // [1, pos] goes to the left tree, remaining goes to the right
+    void split(int t, int& l, int& r, int pos, int add=0) {
+        if (!t) {l = r = 0; return;}
+        pushDown(t);
+        Node &node = nodes_[t];
+        int curr_pos = add + sz(node.l);
+        if (curr_pos < pos) { // element at pos goes to left subtree
+            split(node.r, node.r, r, pos, curr_pos+1);
+            l = t;
+        } else {
+            split(node.l, l, node.l, pos, add);
+            r = t;
+        }
+        pushUp(t);
+    }
+
+    /**
+     * create a node from the memory pool
+     * @param val: value of the node
+     * @return: node index
+     */
+    int allocNode(int val) {
+        Node& node = nodes_[curr_];
+        node.prior = rand();
+        node.size = 1;
+        node.val = val;
+        node.sum = val;
+        node.lazy = 0;
+        node.l = node.r = 0;
+        return curr_++;
+    }
+
+    int build(const vector<int>& a, int i, int j) {
+        // [i, j)
+        if (i == j-1) {
+            return allocNode(a[i]);
+        }
+        int mid = (i+j)/2;
+        int left = build(a, i, mid);
+        int right = build(a, mid, j);
+        int ret;
+        merge(ret, left, right);
+        return ret;
+    }
+};
--- cpp/test/implicit_treap_test.cpp
+++ cpp/test/implicit_treap_test.cpp
@@ -1,7 +1,5 @@
+#include <bits/stdc++.h>
 #include "data_structures/implicit_treap.h"
-
-#include <iostream>
-#include <vector>
 
 #include <boost/assign/list_of.hpp>
 
@@ -42,4 +40,80 @@ public:
     CPPUNIT_TEST_SUITE_END();
 };
 
+class ImplicitTreap2Test:public CppUnit::TestFixture {
+public:
+    void setUp() {}
+    void tearDown() {}
+
+    void testRangeQuery() {
+        ImplicitTreap2 treap(5);
+        vector<int> a = list_of(0)(1)(4)(5)(2)(3);
+        treap.build(a);
+        CPPUNIT_ASSERT_EQUAL(9, treap.rangeQuery(2, 3));
+        CPPUNIT_ASSERT_EQUAL(11, treap.rangeQuery(2, 4));
+    }
+
+    void testRangeUpdate() {
+        ImplicitTreap2 treap(5);
+        vector<int> a = list_of(0)(1)(4)(5)(2)(3);
+        treap.build(a);
+        CPPUNIT_ASSERT_EQUAL(9, treap.rangeQuery(2, 3));
+
+        treap.rangeUpdate(2, 4, 2);
+
+        // 0 1 6 7 4 3
+        CPPUNIT_ASSERT_EQUAL(13, treap.rangeQuery(2, 3));
+        CPPUNIT_ASSERT_EQUAL(17, treap.rangeQuery(2, 4));
+        CPPUNIT_ASSERT_EQUAL(18, treap.rangeQuery(1, 4));
+    }
+
+    void testGet() {
+        ImplicitTreap2 treap(5);
+        vector<int> a = list_of(0)(1)(4)(5)(2)(3);
+        treap.build(a);
+
+        for (int i = 1; i <= 5; i++) {
+            CPPUNIT_ASSERT_EQUAL(a[i], treap[i]);
+        }
+    }
+
+    void testInsert() {
+        ImplicitTreap2 treap(10);
+        vector<int> a = {0, 2, 4};
+        treap.build(a);
+        vector<int> expected =  {0, 2, 4};
+        for (int i = 1; i < (int)expected.size(); i++) {
+            CPPUNIT_ASSERT_EQUAL(expected[i], treap[i]);
+        }
+        treap.insert(0, 1);
+        // now 1, 2, 4
+        expected = {0, 1, 2, 4};
+        for (int i = 1; i < (int)expected.size(); i++) {
+            CPPUNIT_ASSERT_EQUAL(expected[i], treap[i]);
+        }
+
+        treap.insert(3, 5);
+        // now 1, 2, 4, 5
+        expected = {0, 1, 2, 4, 5};
+        for (int i = 1; i < (int)expected.size(); i++) {
+            CPPUNIT_ASSERT_EQUAL(expected[i], treap[i]);
+        }
+
+        treap.insert(2, 3);
+        // now 1, 2, 3, 4, 5
+        expected = {0, 1, 2, 3, 4, 5};
+        for (int i = 1; i < (int)expected.size(); i++) {
+            CPPUNIT_ASSERT_EQUAL(expected[i], treap[i]);
+        }
+    }
+
+    CPPUNIT_TEST_SUITE(ImplicitTreap2Test);
+    CPPUNIT_TEST(testRangeQuery);
+    CPPUNIT_TEST(testRangeUpdate);
+    CPPUNIT_TEST(testGet);
+    CPPUNIT_TEST(testInsert);
+    CPPUNIT_TEST_SUITE_END();
+};
+
 CPPUNIT_TEST_SUITE_REGISTRATION(ImplicitTreapTest);
+CPPUNIT_TEST_SUITE_REGISTRATION(ImplicitTreap2Test);
