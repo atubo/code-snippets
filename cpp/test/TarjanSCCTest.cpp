4b664bd26d99aff71daa72325986a4671c8d2a03
atubo <err722@yahoo.com>
2019-03-03 17:51:51 -0800

    [Tarjan SCC] move file to a new directory

--- cpp/test/TarjanSCCTest.cpp
+++ cpp/test/TarjanSCCTest.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#include "TarjanSCC.h"
+#include "graph/TarjanSCC.h"
 
 #include <boost/assign/list_of.hpp>
 #include <cppunit/extensions/HelperMacros.h>
