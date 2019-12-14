3c02594d352871dc92af00f9970665fe41b5c6aa
atubo <err722@yahoo.com>
2019-12-14 11:39:44 -0800

    [AC automata] re-indent

--- cpp/src/string/aho_corasick.h
+++ cpp/src/string/aho_corasick.h
@@ -23,98 +23,98 @@ using namespace std;
     3. https://en.wikipedia.org/wiki/Aho%E2%80%93Corasick_algorithm
  */
 class AhoCorasick {
-    static constexpr const char *ALPHABET = "abcdefghijklmnopqrstuvwxyz";
-    static const int ALPHA_SIZE = strlen(ALPHABET);
-public:
-    const int n_;
-    int **to;
-    vector<set<int> > out;
-    int sz = 1;
-    int *link;  // blue link in the wiki
-    int *next;  // green link in the wiki
-    int *que;
-    int keywordIndex;
+  static constexpr const char *ALPHABET = "abcdefghijklmnopqrstuvwxyz";
+  static const int ALPHA_SIZE = strlen(ALPHABET);
+ public:
+  const int n_;
+  int **to;
+  vector<set<int> > out;
+  int sz = 1;
+  int *link;  // blue link in the wiki
+  int *next;  // green link in the wiki
+  int *que;
+  int keywordIndex;
 
-    // n is total length, total number of nodes is at most n+1
-    AhoCorasick(int n): n_(n+1), out(n_), keywordIndex(0) {
-        to = new int*[n_]{};
-        for (int i = 0; i < n_; i++) {
-            to[i] = new int[ALPHA_SIZE]{};
-        }
-
-        link = new int[n_]{};
-        next = new int[n_]{};
-        que  = new int[n_]{};
+  // n is total length, total number of nodes is at most n+1
+  AhoCorasick(int n): n_(n+1), out(n_), keywordIndex(0) {
+    to = new int*[n_]{};
+    for (int i = 0; i < n_; i++) {
+      to[i] = new int[ALPHA_SIZE]{};
     }
 
-    ~AhoCorasick() {
-        for (int i = 0; i < n_; i++) {
-            delete[] to[i];
-        }
-        delete[] to;
+    link = new int[n_]{};
+    next = new int[n_]{};
+    que  = new int[n_]{};
+  }
 
-        delete[] link;
-        delete[] next;
-        delete[] que;
+  ~AhoCorasick() {
+    for (int i = 0; i < n_; i++) {
+      delete[] to[i];
     }
+    delete[] to;
 
-    void add_str(const string &s) {
-        int v = 0;
-        for(int i = 0; i < (int)s.length(); i++) {
-            int c = s[i] - ALPHABET[0];
-            if(!to[v][c]) to[v][c] = sz++;
-            v = to[v][c];
-        }
-        out[v].insert(keywordIndex++);
-    }
+    delete[] link;
+    delete[] next;
+    delete[] que;
+  }
 
-    void push_links() {
-        link[0] = -1;
-        int st = 0, fi = 1;
-        que[0] = 0;
-        while (st < fi) {
-            int v = que[st++];
-            for (int c = 0; c < ALPHA_SIZE; c++) {
-                int u = to[v][c];
-                if (u == 0) continue;
-                int j = link[v];
-                while(j != -1 && !to[j][c]) j = link[j];
-                if(j != -1) link[u] = to[j][c];
-                if (out[link[u]].empty()) {
-                    next[u] = next[link[u]];
-                } else {
-                    next[u] = link[u];
-                }
-                que[fi++] = u;
-            }
-        }
+  void add_str(const string &s) {
+    int v = 0;
+    for(int i = 0; i < (int)s.length(); i++) {
+      int c = s[i] - ALPHABET[0];
+      if(!to[v][c]) to[v][c] = sz++;
+      v = to[v][c];
     }
+    out[v].insert(keywordIndex++);
+  }
 
-    int countTotalMatches(const string &t) {
-        int result = 0;
-        int q = 0;
-        for (int i = 0; i < (int)t.length(); i++) {
-            int c = t[i] - ALPHABET[0];
-            while (q != -1 && to[q][c] == 0) {
-                q = link[q];
-            }
-            if (q != -1) {
-                q = to[q][c];
-                result += followDictSuffixLink(q);
-            } else {
-                q = 0;
-            }
+  void push_links() {
+    link[0] = -1;
+    int st = 0, fi = 1;
+    que[0] = 0;
+    while (st < fi) {
+      int v = que[st++];
+      for (int c = 0; c < ALPHA_SIZE; c++) {
+        int u = to[v][c];
+        if (u == 0) continue;
+        int j = link[v];
+        while(j != -1 && !to[j][c]) j = link[j];
+        if(j != -1) link[u] = to[j][c];
+        if (out[link[u]].empty()) {
+          next[u] = next[link[u]];
+        } else {
+          next[u] = link[u];
         }
-        return result;
+        que[fi++] = u;
+      }
     }
+  }
 
-private:
-    int followDictSuffixLink(int q) const {
-        int res = 0;
-        while (q != 0) {
-            res += out[q].size();
-            q = next[q];
-        }
-        return res;
+  int countTotalMatches(const string &t) {
+    int result = 0;
+    int q = 0;
+    for (int i = 0; i < (int)t.length(); i++) {
+      int c = t[i] - ALPHABET[0];
+      while (q != -1 && to[q][c] == 0) {
+        q = link[q];
+      }
+      if (q != -1) {
+        q = to[q][c];
+        result += followDictSuffixLink(q);
+      } else {
+        q = 0;
+      }
     }
+    return result;
+  }
+
+ private:
+  int followDictSuffixLink(int q) const {
+    int res = 0;
+    while (q != 0) {
+      res += out[q].size();
+      q = next[q];
+    }
+    return res;
+  }
 };
--- cpp/test/aho_corasick_test.cpp
+++ cpp/test/aho_corasick_test.cpp
@@ -7,66 +7,66 @@
 using boost::assign::list_of;
 
 class AhoCorasickTest:public CppUnit::TestFixture {
-public:
-    void setUp() {}
-    void tearDown() {}
+ public:
+  void setUp() {}
+  void tearDown() {}
 
-    void testAhoCorasick() {
+  void testAhoCorasick() {
 
-        AhoCorasick ac(12);
-        ac.add_str(string("he"));
-        ac.add_str(string("she"));
-        ac.add_str(string("his"));
-        ac.add_str(string("hers"));
-        ac.push_links();
+    AhoCorasick ac(12);
+    ac.add_str(string("he"));
+    ac.add_str(string("she"));
+    ac.add_str(string("his"));
+    ac.add_str(string("hers"));
+    ac.push_links();
 
-        CPPUNIT_ASSERT(ac.to[0]['s'-'a'] == 3);
-        CPPUNIT_ASSERT(ac.to[2]['r'-'a'] == 8);
-        CPPUNIT_ASSERT(ac.to[4]['e'-'a'] == 5);
+    CPPUNIT_ASSERT(ac.to[0]['s'-'a'] == 3);
+    CPPUNIT_ASSERT(ac.to[2]['r'-'a'] == 8);
+    CPPUNIT_ASSERT(ac.to[4]['e'-'a'] == 5);
 
-        vector<int> link;
-        link.assign(ac.link, ac.link+10);
-        CPPUNIT_ASSERT(link == list_of(-1)(0)(0)(0)(1)(2)(0)(3)(0)(3));
+    vector<int> link;
+    link.assign(ac.link, ac.link+10);
+    CPPUNIT_ASSERT(link == list_of(-1)(0)(0)(0)(1)(2)(0)(3)(0)(3));
 
-        {
-            // verify sizes of out
-            vector<int> expectedSizes = list_of(0)(0)(1)(0)(0)(1)(0)(1)(0)(1);
-            for (int i = 0; i < 10; i++) {
-                CPPUNIT_ASSERT((int)ac.out[i].size() == expectedSizes[i]);
-            }
-        }
-
-        CPPUNIT_ASSERT(ac.out[2] == list_of(0));   // matches "he" which is 1st
-        CPPUNIT_ASSERT(ac.out[5] == list_of(1));
-        CPPUNIT_ASSERT(ac.out[7] == list_of(2));
-        CPPUNIT_ASSERT(ac.out[9] == list_of(3));
-
-        CPPUNIT_ASSERT(ac.countTotalMatches("shershishe") == 6);
+    {
+      // verify sizes of out
+      vector<int> expectedSizes = list_of(0)(0)(1)(0)(0)(1)(0)(1)(0)(1);
+      for (int i = 0; i < 10; i++) {
+        CPPUNIT_ASSERT((int)ac.out[i].size() == expectedSizes[i]);
+      }
     }
 
-    void testDictSuffixLink() {
-        // as in https://en.wikipedia.org/wiki/Aho%E2%80%93Corasick_algorithm
-        AhoCorasick ac(15);
-        ac.add_str("a");
-        ac.add_str("ab");
-        ac.add_str("bab");
-        ac.add_str("bc");
-        ac.add_str("bca");
-        ac.add_str("c");
-        ac.add_str("caa");
-        ac.push_links();
-        vector<int> actual;
-        for (int i = 0; i <= 10; i++) {
-            actual.push_back(ac.next[i]);
-        }
-        vector<int> expected = list_of(0)(0)(0)(0)(1)(2)(8)(1)(0)(1)(1);
-        CPPUNIT_ASSERT(actual == expected);
-    }
+    CPPUNIT_ASSERT(ac.out[2] == list_of(0));   // matches "he" which is 1st
+    CPPUNIT_ASSERT(ac.out[5] == list_of(1));
+    CPPUNIT_ASSERT(ac.out[7] == list_of(2));
+    CPPUNIT_ASSERT(ac.out[9] == list_of(3));
 
-    CPPUNIT_TEST_SUITE(AhoCorasickTest);
-    CPPUNIT_TEST(testAhoCorasick);
-    CPPUNIT_TEST(testDictSuffixLink);
-    CPPUNIT_TEST_SUITE_END();
+    CPPUNIT_ASSERT(ac.countTotalMatches("shershishe") == 6);
+  }
+
+  void testDictSuffixLink() {
+    // as in https://en.wikipedia.org/wiki/Aho%E2%80%93Corasick_algorithm
+    AhoCorasick ac(15);
+    ac.add_str("a");
+    ac.add_str("ab");
+    ac.add_str("bab");
+    ac.add_str("bc");
+    ac.add_str("bca");
+    ac.add_str("c");
+    ac.add_str("caa");
+    ac.push_links();
+    vector<int> actual;
+    for (int i = 0; i <= 10; i++) {
+      actual.push_back(ac.next[i]);
+    }
+    vector<int> expected = list_of(0)(0)(0)(0)(1)(2)(8)(1)(0)(1)(1);
+    CPPUNIT_ASSERT(actual == expected);
+  }
+
+  CPPUNIT_TEST_SUITE(AhoCorasickTest);
+  CPPUNIT_TEST(testAhoCorasick);
+  CPPUNIT_TEST(testDictSuffixLink);
+  CPPUNIT_TEST_SUITE_END();
 };
 
 CPPUNIT_TEST_SUITE_REGISTRATION(AhoCorasickTest);
