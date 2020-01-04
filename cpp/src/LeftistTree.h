38c3813e9aad24006bbbb281af6bc967b799822a
atubo <err722@yahoo.com>
2020-01-04 11:54:24 -0800

    [Leftist Tree] re-indent

--- cpp/src/LeftistTree.h
+++ cpp/src/LeftistTree.h
@@ -1,163 +1,164 @@
 
 
+// By default, top is the min element
 template <class Cmp = less<int>>
 // nodes are 1-indexed
 class LeftistTree {
-public:
+ public:
 
-    LeftistTree(int size) {
-        N = size;
+  LeftistTree(int size) {
+    N = size;
 
-        f = new int[N+1]{};
-        t = new int*[N+1]{};
-        for (int i = 0; i <= N; i++) {
-            t[i] = new int[2]{};
-        }
-        val = new int[N+1]{};
-        d = new int[N+1];
-        memset(d, -1, (N+1) * sizeof(int));
+    f = new int[N+1]{};
+    t = new int*[N+1]{};
+    for (int i = 0; i <= N; i++) {
+      t[i] = new int[2]{};
+    }
+    val = new int[N+1]{};
+    d = new int[N+1];
+    memset(d, -1, (N+1) * sizeof(int));
+  }
+
+  ~LeftistTree() {
+    delete[] f;
+    for (int i = 0; i <= N; i++) {
+      delete[] t[i];
+    }
+    delete[] t;
+    delete[] val;
+    delete[] d;
+  }
+
+  void clear(int m) {
+    int sz = (m + 1) * sizeof(int);
+    memset(f, 0, sz);
+    memset(val, 0, sz);
+    memset(d, -1, sz);
+    for (int i = 0; i <= m; i++) {
+      t[i][0] = t[i][1] = 0;
+    }
+  }
+
+  // build single node
+  void build(int x, int v) {
+    f[x] = 0;
+    t[x][0] = t[x][1] = 0;
+    val[x] = v;
+    d[x] = 0;
+  }
+
+  bool son(int x) const {
+    return t[f[x]][0] == x ? 0 : 1;
+  }
+
+  // push to tree at root, return new root
+  int push(int root, int x, int v) {
+    build(x, v);
+    if (!root) return x;
+    else return merge(root, x);
+  }
+
+  int top(int root) const {
+    assert(!f[root]);
+    return val[root];
+  }
+
+  // must be a root, return new root
+  int pop(int x) {
+    assert(!f[x]);
+    int lc = t[x][0], rc = t[x][1];
+    if (lc) cut(lc);
+    if (rc) cut(rc);
+    if (rc) return merge(lc, rc);
+    else return lc;
+  }
+
+  // merge two trees rooted at x and y, return new root
+  int merge(int x, int y) {
+    assert(x && y);
+    if (cmp(val[y], val[x])) {
+      swap(x, y);
     }
 
-    ~LeftistTree() {
-        delete[] f;
-        for (int i = 0; i <= N; i++) {
-            delete[] t[i];
-        }
-        delete[] t;
-        delete[] val;
-        delete[] d;
+    if (t[x][1] == 0) {
+      connect(x, y, 1);
+    } else if (cmp(val[y], val[t[x][1]])) {
+      int rc = t[x][1];
+      cut(rc);
+      connect(x, y, 1);
+      merge(y, rc);
+    } else {
+      merge(t[x][1], y);
     }
 
-    void clear(int m) {
-        int sz = (m + 1) * sizeof(int);
-        memset(f, 0, sz);
-        memset(val, 0, sz);
-        memset(d, -1, sz);
-        for (int i = 0; i <= m; i++) {
-            t[i][0] = t[i][1] = 0;
-        }
+    if (d[t[x][1]] > d[t[x][0]]) {
+      swap(t[x][0], t[x][1]);
+    }
+    d[x] = d[t[x][1]] + 1;
+    return x;
+  }
+
+  int update(int x, int delta) {
+    // increase val of node x by d
+    int root;
+    if (f[x]) {
+      root = find(x);
+      cut(x);
+      int subroot = pop(x);
+      root = merge(root, subroot);
+    } else {
+      root = pop(x);
     }
 
-    // build single node
-    void build(int x, int v) {
-        f[x] = 0;
-        t[x][0] = t[x][1] = 0;
-        val[x] = v;
-        d[x] = 0;
+    build(x, val[x] + delta);
+    root = merge(root, x);
+    return root;
+  }
+
+  int find(int x) const {
+    while (f[x] != 0) x = f[x];
+    return x;
+  }
+
+  void print() {
+    printf("f: ");
+    for (int i = 0; i <= N; i++) {
+      printf("%d ", f[i]);
     }
-
-    bool son(int x) const {
-        return t[f[x]][0] == x ? 0 : 1;
+    printf("t: ");
+    for (int i = 0; i <= N; i++) {
+      printf("(%d %d) ", t[i][0], t[i][1]);
     }
-
-    // push to tree at root, return new root
-    int push(int root, int x, int v) {
-        build(x, v);
-        if (!root) return x;
-        else return merge(root, x);
+    printf("val: ");
+    for (int i = 0; i <= N; i++) {
+      printf("%d ", val[i]);
     }
-
-    int top(int root) const {
-        assert(!f[root]);
-        return val[root];
+    printf("d: ");
+    for (int i = 0; i <= N; i++) {
+      printf("%d ", d[i]);
     }
+    printf("\n");
+  }
 
-    // must be a root, return new root
-    int pop(int x) {
-        assert(!f[x]);
-        int lc = t[x][0], rc = t[x][1];
-        if (lc) cut(lc);
-        if (rc) cut(rc);
-        if (rc) return merge(lc, rc);
-        else return lc;
-    }
+  int N;  // number of elements, 0 (NULL) excluded
+  int *f;
+  int **t;
+  int *val;
+  int *d;
+  Cmp cmp;
 
-    // merge two trees rooted at x and y, return new root
-    int merge(int x, int y) {
-        assert(x && y);
-        if (cmp(val[y], val[x])) {
-            swap(x, y);
-        }
+ private:
+  // cut x from its parent
+  void cut(int x) {
+    assert(x && f[x] && (t[f[x]][son(x)] == x));
+    t[f[x]][son(x)] = 0;
+    f[x] = 0;
+  }
 
-        if (t[x][1] == 0) {
-            connect(x, y, 1);
-        } else if (cmp(val[y], val[t[x][1]])) {
-            int rc = t[x][1];
-            cut(rc);
-            connect(x, y, 1);
-            merge(y, rc);
-        } else {
-            merge(t[x][1], y);
-        }
-
-        if (d[t[x][1]] > d[t[x][0]]) {
-            swap(t[x][0], t[x][1]);
-        }
-        d[x] = d[t[x][1]] + 1;
-        return x;
-    }
-
-    int update(int x, int delta) {
-        // increase val of node x by d
-        int root;
-        if (f[x]) {
-            root = find(x);
-            cut(x);
-            int subroot = pop(x);
-            root = merge(root, subroot);
-        } else {
-            root = pop(x);
-        }
-
-        build(x, val[x] + delta);
-        root = merge(root, x);
-        return root;
-    }
-
-    int find(int x) const {
-        while (f[x] != 0) x = f[x];
-        return x;
-    }
-
-    void print() {
-        printf("f: ");
-        for (int i = 0; i <= N; i++) {
-            printf("%d ", f[i]);
-        }
-        printf("t: ");
-        for (int i = 0; i <= N; i++) {
-            printf("(%d %d) ", t[i][0], t[i][1]);
-        }
-        printf("val: ");
-        for (int i = 0; i <= N; i++) {
-            printf("%d ", val[i]);
-        }
-        printf("d: ");
-        for (int i = 0; i <= N; i++) {
-            printf("%d ", d[i]);
-        }
-        printf("\n");
-    }
-
-    int N;  // number of elements, 0 (NULL) excluded
-    int *f;
-    int **t;
-    int *val;
-    int *d;
-    Cmp cmp;
-
-private:
-    // cut x from its parent
-    void cut(int x) {
-        assert(x && f[x] && (t[f[x]][son(x)] == x));
-        t[f[x]][son(x)] = 0;
-        f[x] = 0;
-    }
-
-    // connect y as x's child, z = 0 -> left, 1 -> right
-    void connect(int x, int y, int z) {
-        assert(!f[y] && !t[x][z]);
-        f[y] = x;
-        t[x][z] = y;
-    }
+  // connect y as x's child, z = 0 -> left, 1 -> right
+  void connect(int x, int y, int z) {
+    assert(!f[y] && !t[x][z]);
+    f[y] = x;
+    t[x][z] = y;
+  }
 };
--- cpp/test/LeftiestTreeTest.cpp
+++ cpp/test/LeftiestTreeTest.cpp
@@ -7,111 +7,111 @@ using namespace std;
 #include <cppunit/extensions/HelperMacros.h>
 
 class LeftistTreeTest:public CppUnit::TestFixture {
-public:
-    void setUp() {}
-    void tearDown() {}
+ public:
+  void setUp() {}
+  void tearDown() {}
 
-    void testOneTree() {
-        LeftistTree<> lt(5);
-        int root = lt.push(0, 1, 3);
-        root = lt.push(root, 2, 1);
-        root = lt.push(root, 3, 5);
-        CPPUNIT_ASSERT(check(lt,
-                             {0, 2, 0, 2, 0, 0},
-                             {{0, 0}, {0, 0}, {1, 3}, {0, 0}, {0, 0}, {0, 0}},
-                             {0, 3, 1, 5,  0, 0},
-                             {-1, 0, 1, 0, -1, -1}));
+  void testOneTree() {
+    LeftistTree<> lt(5);
+    int root = lt.push(0, 1, 3);
+    root = lt.push(root, 2, 1);
+    root = lt.push(root, 3, 5);
+    CPPUNIT_ASSERT(check(lt,
+          {0, 2, 0, 2, 0, 0},
+          {{0, 0}, {0, 0}, {1, 3}, {0, 0}, {0, 0}, {0, 0}},
+          {0, 3, 1, 5,  0, 0},
+          {-1, 0, 1, 0, -1, -1}));
 
-        root = lt.push(root, 4, 4);
-        root = lt.push(root, 5, 2);
-        CPPUNIT_ASSERT(check(lt,
-                             {0, 2, 0, 4, 5, 2},
-                             {{0, 0}, {0, 0}, {1, 5}, {0, 0}, {3, 0}, {4, 0}},
-                             {0, 3, 1, 5, 4, 2},
-                             {-1, 0, 1, 0, 0, 0}));
+    root = lt.push(root, 4, 4);
+    root = lt.push(root, 5, 2);
+    CPPUNIT_ASSERT(check(lt,
+          {0, 2, 0, 4, 5, 2},
+          {{0, 0}, {0, 0}, {1, 5}, {0, 0}, {3, 0}, {4, 0}},
+          {0, 3, 1, 5, 4, 2},
+          {-1, 0, 1, 0, 0, 0}));
 
-        vector<int> result;
-        for (int i = 0; i < 5; i++) {
-            result.push_back(lt.top(root));
-            root = lt.pop(root);
-        }
-        vector<int> expected = {1, 2, 3, 4, 5};
-        CPPUNIT_ASSERT(result == expected);
+    vector<int> result;
+    for (int i = 0; i < 5; i++) {
+      result.push_back(lt.top(root));
+      root = lt.pop(root);
     }
+    vector<int> expected = {1, 2, 3, 4, 5};
+    CPPUNIT_ASSERT(result == expected);
+  }
 
-    void testMergeTwoTrees() {
-        LeftistTree<> lt(8);
-        int root = lt.push(0, 1, 6);
-        root = lt.push(root, 2, 2);
-        root = lt.push(root, 3, 10);
-        root = lt.push(root, 4, 8);
-        root = lt.push(root, 5, 4);
-        CPPUNIT_ASSERT_EQUAL(2, root);
+  void testMergeTwoTrees() {
+    LeftistTree<> lt(8);
+    int root = lt.push(0, 1, 6);
+    root = lt.push(root, 2, 2);
+    root = lt.push(root, 3, 10);
+    root = lt.push(root, 4, 8);
+    root = lt.push(root, 5, 4);
+    CPPUNIT_ASSERT_EQUAL(2, root);
 
-        root = lt.push(0, 6, 5);
-        root = lt.push(root, 7, 7);
-        root = lt.push(root, 8, 3);
-        CPPUNIT_ASSERT_EQUAL(8, root);
-        CPPUNIT_ASSERT(check(lt,
-                             {0, 2, 0, 4, 5, 2, 8, 6, 0},
-                             {{0, 0}, {0, 0}, {1, 5}, {0, 0}, {3, 0}, {4, 0},
-                              {7, 0}, {0, 0}, {6, 0}},
-                             {0, 6, 2, 10, 8, 4, 5, 7, 3},
-                             {-1, 0, 1, 0, 0, 0, 0, 0, 0}));
+    root = lt.push(0, 6, 5);
+    root = lt.push(root, 7, 7);
+    root = lt.push(root, 8, 3);
+    CPPUNIT_ASSERT_EQUAL(8, root);
+    CPPUNIT_ASSERT(check(lt,
+          {0, 2, 0, 4, 5, 2, 8, 6, 0},
+          {{0, 0}, {0, 0}, {1, 5}, {0, 0}, {3, 0}, {4, 0},
+          {7, 0}, {0, 0}, {6, 0}},
+          {0, 6, 2, 10, 8, 4, 5, 7, 3},
+          {-1, 0, 1, 0, 0, 0, 0, 0, 0}));
 
-        root = lt.merge(8, 2);
-        CPPUNIT_ASSERT_EQUAL(2, root);
-        CPPUNIT_ASSERT(check(lt,
-                             {0, 2, 0, 4, 5, 8, 8, 6, 2},
-                             {{0, 0}, {0, 0}, {8, 1}, {0, 0}, {3, 0}, {4, 0},
-                              {7, 0}, {0, 0}, {6, 5}},
-                             {0, 6, 2, 10, 8, 4, 5, 7, 3},
-                             {-1, 0, 1, 0, 0, 0, 0, 0, 1}));
+    root = lt.merge(8, 2);
+    CPPUNIT_ASSERT_EQUAL(2, root);
+    CPPUNIT_ASSERT(check(lt,
+          {0, 2, 0, 4, 5, 8, 8, 6, 2},
+          {{0, 0}, {0, 0}, {8, 1}, {0, 0}, {3, 0}, {4, 0},
+          {7, 0}, {0, 0}, {6, 5}},
+          {0, 6, 2, 10, 8, 4, 5, 7, 3},
+          {-1, 0, 1, 0, 0, 0, 0, 0, 1}));
 
-        vector<int> result;
-        for (int i = 0; i < 8; i++) {
-            result.push_back(lt.top(root));
-            root = lt.pop(root);
-        }
-        vector<int> expected = {2, 3, 4, 5, 6, 7, 8, 10};
+    vector<int> result;
+    for (int i = 0; i < 8; i++) {
+      result.push_back(lt.top(root));
+      root = lt.pop(root);
     }
+    vector<int> expected = {2, 3, 4, 5, 6, 7, 8, 10};
+  }
 
-    void testUpdateNode() {
-        LeftistTree<> lt(5);
-        int root = lt.push(0, 1, 1);
-        for (int i = 2; i <= 5; i++) {
-            root = lt.push(root, i, i);
-        }
-        CPPUNIT_ASSERT_EQUAL(1, lt.top(root));
-        root = lt.update(1, 6);
-        CPPUNIT_ASSERT_EQUAL(2, lt.top(root));
+  void testUpdateNode() {
+    LeftistTree<> lt(5);
+    int root = lt.push(0, 1, 1);
+    for (int i = 2; i <= 5; i++) {
+      root = lt.push(root, i, i);
     }
+    CPPUNIT_ASSERT_EQUAL(1, lt.top(root));
+    root = lt.update(1, 6);
+    CPPUNIT_ASSERT_EQUAL(2, lt.top(root));
+  }
 
 
-private:
-    bool check(const LeftistTree<> &lt,
-               const vector<int> &f,
-               const vector<array<int, 2>> &t,
-               const vector<int> &val,
-               const vector<int> &d) {
-        int n = f.size();
-        for (int i = 0; i < n; i++) {
-            if (f[i] != lt.f[i] ||
-                t[i][0] != lt.t[i][0] || t[i][1] != lt.t[i][1] ||
-                val[i] != lt.val[i] ||
-                d[i] != lt.d[i]) {
-                return false;
-            }
-        }
-        return true;
-    } 
+ private:
+  bool check(const LeftistTree<> &lt,
+      const vector<int> &f,
+      const vector<array<int, 2>> &t,
+      const vector<int> &val,
+      const vector<int> &d) {
+    int n = f.size();
+    for (int i = 0; i < n; i++) {
+      if (f[i] != lt.f[i] ||
+          t[i][0] != lt.t[i][0] || t[i][1] != lt.t[i][1] ||
+          val[i] != lt.val[i] ||
+          d[i] != lt.d[i]) {
+        return false;
+      }
+    }
+    return true;
+  } 
 
 
-    CPPUNIT_TEST_SUITE(LeftistTreeTest);
-    CPPUNIT_TEST(testOneTree);
-    CPPUNIT_TEST(testMergeTwoTrees);
-    CPPUNIT_TEST(testUpdateNode);
-    CPPUNIT_TEST_SUITE_END();
+  CPPUNIT_TEST_SUITE(LeftistTreeTest);
+  CPPUNIT_TEST(testOneTree);
+  CPPUNIT_TEST(testMergeTwoTrees);
+  CPPUNIT_TEST(testUpdateNode);
+  CPPUNIT_TEST_SUITE_END();
 };
 
 CPPUNIT_TEST_SUITE_REGISTRATION(LeftistTreeTest);
