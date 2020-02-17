c391080e9f096ab6ebedb39250eaca6bfa25f81c
atubo <err722@yahoo.com>
2020-02-17 08:18:39 -0800

    [SegmentInFenwick] re-indent

--- cpp/src/data_structures/segment_in_fenwick.h
+++ cpp/src/data_structures/segment_in_fenwick.h
@@ -1,62 +1,62 @@
 struct SegmentInFenwick {
-    int N;  // number of positions, index is 1-based
-    int M;  // number of weights, index is 0-based
-    int T_cnt;
-    int *root;
-    int *val, *lc, *rc;
+  int N;  // number of positions, index is 1-based
+  int M;  // number of weights, index is 0-based
+  int T_cnt;
+  int *root;
+  int *val, *lc, *rc;
 
-    // cap is the number of elements to be added
-    SegmentInFenwick(int N_, int M_, int cap): N(N_), M(M_), T_cnt(1) {
-        root = new int[N+1]{};
-        const int numOfNodes = cap * int(log2(N)+2) * int(log2(M)+2);
-        val = new int[numOfNodes]{};
-        lc = new int[numOfNodes]{};
-        rc = new int[numOfNodes]{};
+  // cap is the number of elements to be added
+  SegmentInFenwick(int N_, int M_, int cap): N(N_), M(M_), T_cnt(1) {
+    root = new int[N+1]{};
+    const int numOfNodes = cap * int(log2(N)+2) * int(log2(M)+2);
+    val = new int[numOfNodes]{};
+    lc = new int[numOfNodes]{};
+    rc = new int[numOfNodes]{};
+  }
+
+  ~SegmentInFenwick() {
+    delete[] root;
+    delete[] val;
+    delete[] lc;
+    delete[] rc;
+  }
+
+  // add value t to weight node w at position x
+  void add(int x, int w, int t) {
+    for (int i = x; i <= N; i += lowbit(i)) {
+      update(root[i], w, t, 0, M-1);
     }
+  }
 
-    ~SegmentInFenwick() {
-        delete[] root;
-        delete[] val;
-        delete[] lc;
-        delete[] rc;
+  // weight sum [l, r] for position [1, k]
+  int query(int k, int l, int r) const {
+    if (l > r) return 0;
+    int sum = 0;
+    for (int j = k; j; j -= lowbit(j)) {
+      sum += querySingleTree(root[j], 0, M-1, l, r);
     }
+    return sum;
+  }
 
-    // add value t to weight node w at position x
-    void add(int x, int w, int t) {
-        for (int i = x; i <= N; i += lowbit(i)) {
-            update(root[i], w, t, 0, M-1);
-        }
-    }
+ private:
+  int lowbit(int x) const {return x & -x;}
 
-    // weight sum [l, r] for position [1, k]
-    int query(int k, int l, int r) const {
-        if (l > r) return 0;
-        int sum = 0;
-        for (int j = k; j; j -= lowbit(j)) {
-            sum += querySingleTree(root[j], 0, M-1, l, r);
-        }
-        return sum;
-    }
+  void update(int &now, int w, int t, int l, int r) {
+    if (!now) now = T_cnt++;
+    val[now] += t;
+    if (l == r) return;
+    int mid = (l + r) / 2;
+    if (w <= mid) update(lc[now], w, t, l, mid);
+    else          update(rc[now], w, t, mid+1, r);
+  }
 
-private:
-    int lowbit(int x) const {return x & -x;}
-
-    void update(int &now, int w, int t, int l, int r) {
-        if (!now) now = T_cnt++;
-        val[now] += t;
-        if (l == r) return;
-        int mid = (l + r) / 2;
-        if (w <= mid) update(lc[now], w, t, l, mid);
-        else          update(rc[now], w, t, mid+1, r);
-    }
-
-    int querySingleTree(int node, int L, int R, int l, int r) const {
-        if (L >= l && R <= r) return val[node];
-        if (L > r || R < l) return 0;
-        int sum = 0;
-        int mid = (L + R) / 2;
-        if (l <= mid) sum += querySingleTree(lc[node], L, mid, l, r);
-        if (mid < r)  sum += querySingleTree(rc[node], mid+1, R, l, r);
-        return sum;
-    }
+  int querySingleTree(int node, int L, int R, int l, int r) const {
+    if (L >= l && R <= r) return val[node];
+    if (L > r || R < l) return 0;
+    int sum = 0;
+    int mid = (L + R) / 2;
+    if (l <= mid) sum += querySingleTree(lc[node], L, mid, l, r);
+    if (mid < r)  sum += querySingleTree(rc[node], mid+1, R, l, r);
+    return sum;
+  }
 };
--- cpp/test/segment_in_fenwick_test.cpp
+++ cpp/test/segment_in_fenwick_test.cpp
@@ -7,37 +7,37 @@ using namespace std;
 #include <cppunit/extensions/HelperMacros.h>
 
 class SegmentInFenwickTest:public CppUnit::TestFixture {
-public:
-    void setUp() {}
-    void tearDown() {}
+ public:
+  void setUp() {}
+  void tearDown() {}
 
-    void test() {
-        SegmentInFenwick tree(4, 4, 4);
-        tree.add(1, 2, 2);
-        tree.add(2, 1, 1);
-        tree.add(3, 3, 3);
-        tree.add(4, 0, 4);
-        CPPUNIT_ASSERT_EQUAL(3, tree.query(2, 1, 2));
-        CPPUNIT_ASSERT_EQUAL(5, tree.query(3, 2, 3));
-        CPPUNIT_ASSERT_EQUAL(10, tree.query(4, 0, 3));
-    }
+  void test() {
+    SegmentInFenwick tree(4, 4, 4);
+    tree.add(1, 2, 2);
+    tree.add(2, 1, 1);
+    tree.add(3, 3, 3);
+    tree.add(4, 0, 4);
+    CPPUNIT_ASSERT_EQUAL(3, tree.query(2, 1, 2));
+    CPPUNIT_ASSERT_EQUAL(5, tree.query(3, 2, 3));
+    CPPUNIT_ASSERT_EQUAL(10, tree.query(4, 0, 3));
+  }
 
-    void testWeightIndexZero() {
-        SegmentInFenwick tree(2, 2, 4);
-        tree.add(1, 0, 1);
-        tree.add(1, 1, 2);
-        tree.add(2, 0, 3);
-        tree.add(2, 1, 4);
-        CPPUNIT_ASSERT_EQUAL(1, tree.query(1, 0, 0));
-        CPPUNIT_ASSERT_EQUAL(3, tree.query(1, 0, 1));
-        CPPUNIT_ASSERT_EQUAL(4, tree.query(2, 0, 0));
-        CPPUNIT_ASSERT_EQUAL(10, tree.query(2, 0, 1));
-    }
+  void testWeightIndexZero() {
+    SegmentInFenwick tree(2, 2, 4);
+    tree.add(1, 0, 1);
+    tree.add(1, 1, 2);
+    tree.add(2, 0, 3);
+    tree.add(2, 1, 4);
+    CPPUNIT_ASSERT_EQUAL(1, tree.query(1, 0, 0));
+    CPPUNIT_ASSERT_EQUAL(3, tree.query(1, 0, 1));
+    CPPUNIT_ASSERT_EQUAL(4, tree.query(2, 0, 0));
+    CPPUNIT_ASSERT_EQUAL(10, tree.query(2, 0, 1));
+  }
 
-    CPPUNIT_TEST_SUITE(SegmentInFenwickTest);
-    CPPUNIT_TEST(test);
-    CPPUNIT_TEST(testWeightIndexZero);
-    CPPUNIT_TEST_SUITE_END();
+  CPPUNIT_TEST_SUITE(SegmentInFenwickTest);
+  CPPUNIT_TEST(test);
+  CPPUNIT_TEST(testWeightIndexZero);
+  CPPUNIT_TEST_SUITE_END();
 };
 
 CPPUNIT_TEST_SUITE_REGISTRATION(SegmentInFenwickTest);
