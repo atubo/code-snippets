6b5e10213b84a59cb38f047d73724a2f74d9fba9
atubo <err722@yahoo.com>
2020-03-01 14:39:04 -0800

    [Lichao segment tree]: re-indent

--- cpp/test/lichao_segment_tree_test.cpp
+++ cpp/test/lichao_segment_tree_test.cpp
@@ -7,39 +7,38 @@ using namespace std;
 #include <cppunit/extensions/HelperMacros.h>
 
 class LcSegmentTreeTest:public CppUnit::TestFixture {
-public:
-    void setUp() {}
-    void tearDown() {}
+ public:
+  void setUp() {}
+  void tearDown() {}
 
-    void testNormal() {
-        LcSegmentTree lc(8);
-        lc.modify(0.5, 1, 1, 4);
-        lc.modify(0.875, -1, 1, 8);
-        lc.modify(0.125, 2.1, 1, 8);
-        vector<double> expected{0, 2.225, 2.35, 2.5, 3.0,
-            3.375, 4.25, 5.125, 6};
-        for (int i = 1; i < 9; i++) {
-            CPPUNIT_ASSERT_DOUBLES_EQUAL(expected[i], lc.query(i), 1e-9);
-        }
+  void testNormal() {
+    LcSegmentTree lc(8);
+    lc.modify(0.5, 1, 1, 4);
+    lc.modify(0.875, -1, 1, 8);
+    lc.modify(0.125, 2.1, 1, 8);
+    vector<double> expected{0, 2.225, 2.35, 2.5, 3.0,
+      3.375, 4.25, 5.125, 6};
+    for (int i = 1; i < 9; i++) {
+      CPPUNIT_ASSERT_DOUBLES_EQUAL(expected[i], lc.query(i), 1e-9);
     }
+  }
 
-    void testCrossInMiddle() {
-        LcSegmentTree lc(8);
-        lc.modify(0.5, 1, 1, 4);
-        lc.modify(0.875, -1, 1, 8);
-        lc.modify(0.125, 2, 1, 8);
-        vector<double> expected{0, 2.125, 2.25, 2.5, 3.0,
-            3.375, 4.25, 5.125, 6};
-        for (int i = 1; i < 9; i++) {
-            CPPUNIT_ASSERT_DOUBLES_EQUAL(expected[i], lc.query(i), 1e-9);
-        }
+  void testCrossInMiddle() {
+    LcSegmentTree lc(8);
+    lc.modify(0.5, 1, 1, 4);
+    lc.modify(0.875, -1, 1, 8);
+    lc.modify(0.125, 2, 1, 8);
+    vector<double> expected{0, 2.125, 2.25, 2.5, 3.0,
+      3.375, 4.25, 5.125, 6};
+    for (int i = 1; i < 9; i++) {
+      CPPUNIT_ASSERT_DOUBLES_EQUAL(expected[i], lc.query(i), 1e-9);
     }
+  }
 
-
-    CPPUNIT_TEST_SUITE(LcSegmentTreeTest);
-    CPPUNIT_TEST(testNormal);
-    CPPUNIT_TEST(testCrossInMiddle);
-    CPPUNIT_TEST_SUITE_END();
+  CPPUNIT_TEST_SUITE(LcSegmentTreeTest);
+  CPPUNIT_TEST(testNormal);
+  CPPUNIT_TEST(testCrossInMiddle);
+  CPPUNIT_TEST_SUITE_END();
 };
 
 CPPUNIT_TEST_SUITE_REGISTRATION(LcSegmentTreeTest);
