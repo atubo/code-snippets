e439146d405cbdc883202c0e8358255303554f8c
atubo <err722@yahoo.com>
2020-03-22 15:36:09 -0700

    [Persistent segment tree] re-indent

--- cpp/src/data_structures/persistent_segment_tree.h
+++ cpp/src/data_structures/persistent_segment_tree.h
@@ -1,62 +1,62 @@
 class PersistentSegmentTree {
-public:
-    struct Node {
-        // L and R are indices of its children
-        // real left and right boundaries are not kept in the node
-        // to save memory
-        int L, R;
-        int sum;
+ public:
+  struct Node {
+    // L and R are indices of its children
+    // real left and right boundaries are not kept in the node
+    // to save memory
+    int L, R;
+    int sum;
 
-        Node(): L(0), R(0), sum(0) {}
-    };
+    Node(): L(0), R(0), sum(0) {}
+  };
 
-    int N; // number of positions, index is 1-based
-    int M; // number of weights, index is 0-based
-    int *root;
-    Node *T;
-    int T_cnt = 1;
+  int N; // number of positions, index is 1-based
+  int M; // number of weights, index is 0-based
+  int *root;
+  Node *T;
+  int T_cnt = 1;
 
-    PersistentSegmentTree(int N_, int M_): N(N_), M(M_) {
-        root = new int[N+1]{};
-        T = new Node[(N+1) * int(log2(M)+2)]{};
-    }
+  PersistentSegmentTree(int N_, int M_): N(N_), M(M_) {
+    root = new int[N+1]{};
+    T = new Node[(N+1) * int(log2(M)+2)]{};
+  }
 
-    ~PersistentSegmentTree() {
-        delete[] root;
-        delete[] T;
-    }
+  ~PersistentSegmentTree() {
+    delete[] root;
+    delete[] T;
+  }
 
-    // for user's convenience
-    void insert(int currRootIndex, int prevRootIndex, int x, int t) {
-        root[currRootIndex] = root[prevRootIndex];
-        insert(root[currRootIndex], x, t, 0, M-1);
-    }
+  // for user's convenience
+  void insert(int currRootIndex, int prevRootIndex, int x, int t) {
+    root[currRootIndex] = root[prevRootIndex];
+    insert(root[currRootIndex], x, t, 0, M-1);
+  }
 
-    // query in range [start, end]
-    int query(int rootIndex, int start, int end) const {
-        return query(root[rootIndex], start, end, 0, M-1);
-    }
+  // query in range [start, end]
+  int query(int rootIndex, int start, int end) const {
+    return query(root[rootIndex], start, end, 0, M-1);
+  }
 
-private:
-    // insert a new weight node x with value t
-    // current range is [l, r]
-    void insert(int &now, int x, int t, int l, int r) {
-        T[T_cnt++] = T[now];
-        now = T_cnt-1;
-        T[now].sum += t;
-        if (l == r) return;
-        int mid = (l + r) / 2;
-        if (x <= mid) insert(T[now].L, x, t, l, mid);
-        else          insert(T[now].R, x, t, mid+1, r);
-    }
+ private:
+  // insert a new weight node x with value t
+  // current range is [l, r]
+  void insert(int &now, int x, int t, int l, int r) {
+    T[T_cnt++] = T[now];
+    now = T_cnt-1;
+    T[now].sum += t;
+    if (l == r) return;
+    int mid = (l + r) / 2;
+    if (x <= mid) insert(T[now].L, x, t, l, mid);
+    else          insert(T[now].R, x, t, mid+1, r);
+  }
 
-    int query(int node, int start, int end, int l, int r) const {
-        if (l > r) return 0;
-        if (start <= l && r <= end) return T[node].sum;
-        int mid = (l + r) / 2;
-        int ret = 0;
-        if (start <= mid) ret += query(T[node].L, start, end, l, mid);
-        if (mid+1 <= end) ret += query(T[node].R, start, end, mid+1, r);
-        return ret;
-    }
+  int query(int node, int start, int end, int l, int r) const {
+    if (l > r) return 0;
+    if (start <= l && r <= end) return T[node].sum;
+    int mid = (l + r) / 2;
+    int ret = 0;
+    if (start <= mid) ret += query(T[node].L, start, end, l, mid);
+    if (mid+1 <= end) ret += query(T[node].R, start, end, mid+1, r);
+    return ret;
+  }
 };
--- cpp/test/persistent_segment_tree_test.cpp
+++ cpp/test/persistent_segment_tree_test.cpp
@@ -7,25 +7,25 @@ using namespace std;
 #include <cppunit/extensions/HelperMacros.h>
 
 class PersistentSegmentTreeTest:public CppUnit::TestFixture {
-public:
-    void setUp() {}
-    void tearDown() {}
+ public:
+  void setUp() {}
+  void tearDown() {}
 
-    void test() {
-        PersistentSegmentTree pst(4, 4);
-        pst.insert(1, 0, 2, 1);
-        pst.insert(2, 1, 1, 1);
-        pst.insert(3, 2, 3, 1);
-        pst.insert(4, 3, 0, 1);
-        CPPUNIT_ASSERT_EQUAL(2, pst.query(2, 1, 2));
-        CPPUNIT_ASSERT_EQUAL(1, pst.query(2, 0, 1));
-        CPPUNIT_ASSERT_EQUAL(3, pst.query(3, 1, 3));
-        CPPUNIT_ASSERT_EQUAL(2, pst.query(4, 2, 3));
-    }
+  void test() {
+    PersistentSegmentTree pst(4, 4);
+    pst.insert(1, 0, 2, 1);
+    pst.insert(2, 1, 1, 1);
+    pst.insert(3, 2, 3, 1);
+    pst.insert(4, 3, 0, 1);
+    CPPUNIT_ASSERT_EQUAL(2, pst.query(2, 1, 2));
+    CPPUNIT_ASSERT_EQUAL(1, pst.query(2, 0, 1));
+    CPPUNIT_ASSERT_EQUAL(3, pst.query(3, 1, 3));
+    CPPUNIT_ASSERT_EQUAL(2, pst.query(4, 2, 3));
+  }
 
-    CPPUNIT_TEST_SUITE(PersistentSegmentTreeTest);
-    CPPUNIT_TEST(test);
-    CPPUNIT_TEST_SUITE_END();
+  CPPUNIT_TEST_SUITE(PersistentSegmentTreeTest);
+  CPPUNIT_TEST(test);
+  CPPUNIT_TEST_SUITE_END();
 };
 
 CPPUNIT_TEST_SUITE_REGISTRATION(PersistentSegmentTreeTest);
