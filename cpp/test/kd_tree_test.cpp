f6a488d48b09a628d0e64e36c059b2363bc401d4
atubo <err722@yahoo.com>
2020-04-04 14:04:09 -0700

    [KD tree] re-indent

--- cpp/test/kd_tree_test.cpp
+++ cpp/test/kd_tree_test.cpp
@@ -7,23 +7,23 @@ using namespace std;
 #include <cppunit/extensions/HelperMacros.h>
 
 class KdTreeTest:public CppUnit::TestFixture {
-public:
-    void setUp() {}
-    void tearDown() {}
+ public:
+  void setUp() {}
+  void tearDown() {}
 
-    void test() {
-        KdTree kd(2);
-        kd.insert(2, 3, 3);
-        int ret = kd.query(1, 1, 3, 3);
-        CPPUNIT_ASSERT_EQUAL(3, ret);
-        kd.insert(2, 2, 2);
-        ret = kd.query(2, 2, 3, 4);
-        CPPUNIT_ASSERT_EQUAL(5, ret);
-    }
+  void test() {
+    KdTree kd(2);
+    kd.insert(2, 3, 3);
+    int ret = kd.query(1, 1, 3, 3);
+    CPPUNIT_ASSERT_EQUAL(3, ret);
+    kd.insert(2, 2, 2);
+    ret = kd.query(2, 2, 3, 4);
+    CPPUNIT_ASSERT_EQUAL(5, ret);
+  }
 
-    CPPUNIT_TEST_SUITE(KdTreeTest);
-    CPPUNIT_TEST(test);
-    CPPUNIT_TEST_SUITE_END();
+  CPPUNIT_TEST_SUITE(KdTreeTest);
+  CPPUNIT_TEST(test);
+  CPPUNIT_TEST_SUITE_END();
 };
 
 CPPUNIT_TEST_SUITE_REGISTRATION(KdTreeTest);
