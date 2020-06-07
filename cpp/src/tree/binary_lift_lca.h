a3c0a863ee2cc582d3787a8949a0ba8610f4c33b
atubo <err722@yahoo.com>
2020-06-07 15:28:16 -0700

    [Binary lift LCA] re-indent

--- cpp/src/tree/binary_lift_lca.h
+++ cpp/src/tree/binary_lift_lca.h
@@ -2,125 +2,125 @@
 // Each query O(log N)
 
 class BinaryLiftLCA {
-    // Note graph node is 0-indexed
-    class Graph {
-    public:
-        struct Edge {
-            int next, to;
-        };
-
-        vector<int> head;
-        int eidx;
-        int N, M;
-
-        Edge *E;
-
-        Graph(int N_, int M_):N(N_), M(M_) {
-            head.resize(N);
-            eidx = 0;
-
-            for (int i = 0; i < N; i++) {
-                head[i] = -1;
-            }
-
-            E = new Edge[M]{};
-        }
-
-        ~Graph() {
-            delete[] E;
-        }
-
-        // assume 0-indexed and no duplication
-        void addEdge(int u, int v) {
-            E[eidx].to = v;
-            E[eidx].next = head[u];
-            head[u] = eidx++;
-        }
+  // Note graph node is 0-indexed
+  class Graph {
+   public:
+    struct Edge {
+      int next, to;
     };
 
-    int N_, root_;
-    int MAXB_;
-    Graph g_;
-public:
-    int* depth;
-    int** father;
+    vector<int> head;
+    int eidx;
+    int N, M;
 
-public:
-    BinaryLiftLCA(int N, int root)
-        : N_(N), root_(root), MAXB_(log2(N)+1), g_(N, 2*(N-1)) {
-            alloc();
+    Edge *E;
+
+    Graph(int N_, int M_):N(N_), M(M_) {
+      head.resize(N);
+      eidx = 0;
+
+      for (int i = 0; i < N; i++) {
+        head[i] = -1;
+      }
+
+      E = new Edge[M]{};
     }
 
-    ~BinaryLiftLCA() {
-        dealloc();
+    ~Graph() {
+      delete[] E;
     }
 
+    // assume 0-indexed and no duplication
     void addEdge(int u, int v) {
-        g_.addEdge(u, v);
-        g_.addEdge(v, u);
+      E[eidx].to = v;
+      E[eidx].next = head[u];
+      head[u] = eidx++;
+    }
+  };
+
+  int N_, root_;
+  int MAXB_;
+  Graph g_;
+ public:
+  int* depth;
+  int** father;
+
+ public:
+  BinaryLiftLCA(int N, int root)
+      : N_(N), root_(root), MAXB_(log2(N)+1), g_(N, 2*(N-1)) {
+        alloc();
+      }
+
+  ~BinaryLiftLCA() {
+    dealloc();
+  }
+
+  void addEdge(int u, int v) {
+    g_.addEdge(u, v);
+    g_.addEdge(v, u);
+  }
+
+  void build() {
+    dfs(root_, -1, 0);
+
+    binaryLift();
+  }
+
+  int findLCA(int u, int v) const {
+    if (depth[u] < depth[v]) swap(u, v);
+    for (int b = MAXB_-1; b >= 0; b--) {
+      if (father[u][b] == -1) continue;
+      if (depth[father[u][b]] >= depth[v]) {
+        u = father[u][b];
+      }
     }
 
-    void build() {
-        dfs(root_, -1, 0);
+    if (u == v) return u;
 
-        binaryLift();
+    for (int b = MAXB_-1; b >= 0; b--) {
+      if (father[u][b] == -1) continue;
+      if (father[u][b] != father[v][b]) {
+        u = father[u][b];
+        v = father[v][b];
+      }
     }
+    return father[u][0];
+  }
 
-    int findLCA(int u, int v) const {
-        if (depth[u] < depth[v]) swap(u, v);
-        for (int b = MAXB_-1; b >= 0; b--) {
-            if (father[u][b] == -1) continue;
-            if (depth[father[u][b]] >= depth[v]) {
-                u = father[u][b];
-            }
+ private:
+  void alloc() {
+    depth = new int[N_]{};
+    father = new int*[N_]{};
+    for (int i = 0; i < N_; i++) {
+      father[i] = new int[MAXB_]{};
+      memset(father[i], -1, MAXB_*sizeof(int));
+    }
+  }
+
+  void dealloc() {
+    delete[] depth;
+    for (int i = 0; i < N_; i++) {
+      delete[] father[i];
+    }
+    delete[] father;
+  }
+
+  void dfs(int x, int f, int d) {
+    depth[x] = d;
+    father[x][0] = f;
+    for (int eidx = g_.head[x]; ~eidx; eidx = g_.E[eidx].next) {
+      int u = g_.E[eidx].to;
+      if (u != f) dfs(u, x, d+1);
+    }
+  }
+
+  void binaryLift() {
+    for (int j = 1; j < MAXB_; j++) {
+      for (int i = 0; i < N_; i++) {
+        if (father[i][j-1] != -1) {
+          father[i][j] = father[father[i][j-1]][j-1];
         }
-
-        if (u == v) return u;
-
-        for (int b = MAXB_-1; b >= 0; b--) {
-            if (father[u][b] == -1) continue;
-            if (father[u][b] != father[v][b]) {
-                u = father[u][b];
-                v = father[v][b];
-            }
-        }
-        return father[u][0];
+      }
     }
-
-private:
-    void alloc() {
-        depth = new int[N_]{};
-        father = new int*[N_]{};
-        for (int i = 0; i < N_; i++) {
-            father[i] = new int[MAXB_]{};
-            memset(father[i], -1, MAXB_*sizeof(int));
-        }
-    }
-
-    void dealloc() {
-        delete[] depth;
-        for (int i = 0; i < N_; i++) {
-            delete[] father[i];
-        }
-        delete[] father;
-    }
-
-    void dfs(int x, int f, int d) {
-        depth[x] = d;
-        father[x][0] = f;
-        for (int eidx = g_.head[x]; ~eidx; eidx = g_.E[eidx].next) {
-            int u = g_.E[eidx].to;
-            if (u != f) dfs(u, x, d+1);
-        }
-    }
-
-    void binaryLift() {
-        for (int j = 1; j < MAXB_; j++) {
-            for (int i = 0; i < N_; i++) {
-                if (father[i][j-1] != -1) {
-                    father[i][j] = father[father[i][j-1]][j-1];
-                }
-            }
-        }
-    }
+  }
 };
--- cpp/test/binary_lift_lca_test.cpp
+++ cpp/test/binary_lift_lca_test.cpp
@@ -10,49 +10,49 @@ using namespace std;
 using namespace boost::assign;
 
 class BinaryLiftLCATest:public CppUnit::TestFixture {
-public:
-    void setUp() {}
-    void tearDown() {}
+ public:
+  void setUp() {}
+  void tearDown() {}
 
-    void testLCA() {
-        const int N = 7;
-        BinaryLiftLCA lca(N, 0);
-        lca.addEdge(0, 1);
-        lca.addEdge(0, 2);
-        lca.addEdge(1, 3);
-        lca.addEdge(1, 4);
-        lca.addEdge(2, 5);
-        lca.addEdge(2, 6);
+  void testLCA() {
+    const int N = 7;
+    BinaryLiftLCA lca(N, 0);
+    lca.addEdge(0, 1);
+    lca.addEdge(0, 2);
+    lca.addEdge(1, 3);
+    lca.addEdge(1, 4);
+    lca.addEdge(2, 5);
+    lca.addEdge(2, 6);
 
-        lca.build();
+    lca.build();
 
-        CPPUNIT_ASSERT(buildVec(lca.depth, 7) == list_of(0)(1)(1)(2)(2)(2)(2));
-        CPPUNIT_ASSERT(buildVec(lca.father[0], 3) == list_of(-1)(-1)(-1));
-        CPPUNIT_ASSERT(buildVec(lca.father[1], 3) == list_of(0)(-1)(-1));
-        CPPUNIT_ASSERT(buildVec(lca.father[2], 3) == list_of(0)(-1)(-1));
-        CPPUNIT_ASSERT(buildVec(lca.father[3], 3) == list_of(1)(0)(-1));
-        CPPUNIT_ASSERT(buildVec(lca.father[4], 3) == list_of(1)(0)(-1));
-        CPPUNIT_ASSERT(buildVec(lca.father[5], 3) == list_of(2)(0)(-1));
-        CPPUNIT_ASSERT(buildVec(lca.father[6], 3) == list_of(2)(0)(-1));
+    CPPUNIT_ASSERT(buildVec(lca.depth, 7) == list_of(0)(1)(1)(2)(2)(2)(2));
+    CPPUNIT_ASSERT(buildVec(lca.father[0], 3) == list_of(-1)(-1)(-1));
+    CPPUNIT_ASSERT(buildVec(lca.father[1], 3) == list_of(0)(-1)(-1));
+    CPPUNIT_ASSERT(buildVec(lca.father[2], 3) == list_of(0)(-1)(-1));
+    CPPUNIT_ASSERT(buildVec(lca.father[3], 3) == list_of(1)(0)(-1));
+    CPPUNIT_ASSERT(buildVec(lca.father[4], 3) == list_of(1)(0)(-1));
+    CPPUNIT_ASSERT(buildVec(lca.father[5], 3) == list_of(2)(0)(-1));
+    CPPUNIT_ASSERT(buildVec(lca.father[6], 3) == list_of(2)(0)(-1));
 
-        CPPUNIT_ASSERT_EQUAL(0, lca.findLCA(0, 0));
-        CPPUNIT_ASSERT_EQUAL(0, lca.findLCA(0, 4));
-        CPPUNIT_ASSERT_EQUAL(1, lca.findLCA(1, 4));
-        CPPUNIT_ASSERT_EQUAL(0, lca.findLCA(3, 5));
-        CPPUNIT_ASSERT_EQUAL(1, lca.findLCA(3, 4));
+    CPPUNIT_ASSERT_EQUAL(0, lca.findLCA(0, 0));
+    CPPUNIT_ASSERT_EQUAL(0, lca.findLCA(0, 4));
+    CPPUNIT_ASSERT_EQUAL(1, lca.findLCA(1, 4));
+    CPPUNIT_ASSERT_EQUAL(0, lca.findLCA(3, 5));
+    CPPUNIT_ASSERT_EQUAL(1, lca.findLCA(3, 4));
+  }
+
+  vector<int> buildVec(int *a, int n) {
+    vector<int> ret;
+    for (int i = 0; i < n; i++) {
+      ret.push_back(a[i]);
     }
+    return ret;
+  }
 
-    vector<int> buildVec(int *a, int n) {
-        vector<int> ret;
-        for (int i = 0; i < n; i++) {
-            ret.push_back(a[i]);
-        }
-        return ret;
-    }
-
-    CPPUNIT_TEST_SUITE(BinaryLiftLCATest);
-    CPPUNIT_TEST(testLCA);
-    CPPUNIT_TEST_SUITE_END();
+  CPPUNIT_TEST_SUITE(BinaryLiftLCATest);
+  CPPUNIT_TEST(testLCA);
+  CPPUNIT_TEST_SUITE_END();
 };
 
 CPPUNIT_TEST_SUITE_REGISTRATION(BinaryLiftLCATest);
