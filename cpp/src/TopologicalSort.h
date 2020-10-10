906924b00e5a5dd38cfadcc2781e1904864b18d5
atubo <err722@yahoo.com>
2020-10-10 16:28:20 -0700

    [Toposort] reindent

--- cpp/src/TopologicalSort.h
+++ cpp/src/TopologicalSort.h
@@ -1,48 +1,48 @@
 
 // Note: be sure it's DAG
 class TopologicalSort {
-public:
-    vector<int> topo;  // topologically sorted result
+ public:
+  vector<int> topo;  // topologically sorted result
 
-private:
-    void topologicalSortUtil(const Graph &g, int v, bool visited[],
-                             stack<int> &order) {
-        visited[v] = true;
+ private:
+  void topologicalSortUtil(const Graph &g, int v, bool visited[],
+      stack<int> &order) {
+    visited[v] = true;
 
-        for (int i = g.head[v]; i != -1; i = g.E[i].next) {
-            int m = g.E[i].to;
-            if (!visited[m]) {
-                topologicalSortUtil(g, m, visited, order);
-            }
-        }
-
-        order.push(v);
+    for (int i = g.head[v]; i != -1; i = g.E[i].next) {
+      int m = g.E[i].to;
+      if (!visited[m]) {
+        topologicalSortUtil(g, m, visited, order);
+      }
     }
 
-public:
-    TopologicalSort() {}
+    order.push(v);
+  }
 
-    void sort(const Graph &g) {
-        const int N = g.N;
-        topo.resize(N);
+ public:
+  TopologicalSort() {}
 
-        stack<int> order;
-        bool visited[N];
-        for (int i = 0; i < N; i++) {
-            visited[i] = false;
-        }
+  void sort(const Graph &g) {
+    const int N = g.N;
+    topo.resize(N);
 
-        for (int i = 0; i < N; i++) {
-            if (!visited[i]) {
-                topologicalSortUtil(g, i, visited, order);
-            }
-        }
-
-        int i = 0;
-        while (!order.empty()) {
-            topo[i++] = order.top();
-            order.pop();
-        }
+    stack<int> order;
+    bool visited[N];
+    for (int i = 0; i < N; i++) {
+      visited[i] = false;
     }
+
+    for (int i = 0; i < N; i++) {
+      if (!visited[i]) {
+        topologicalSortUtil(g, i, visited, order);
+      }
+    }
+
+    int i = 0;
+    while (!order.empty()) {
+      topo[i++] = order.top();
+      order.pop();
+    }
+  }
 };
 
