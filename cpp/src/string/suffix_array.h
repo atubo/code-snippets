f06741237437cca37da5010508b48a7ebd9d0fde
atubo <err722@yahoo.com>
2020-12-20 13:19:26 -0800

    [SuffixArray] refactoring

--- cpp/src/string/suffix_array.h
+++ cpp/src/string/suffix_array.h
@@ -7,79 +7,105 @@ LCP table O(n)
 #define REP(i, n) for (int i = 0; i < (int)(n); ++i)
 
 // lcp(i) = LCP(i, i+1)
-namespace SuffixArray
-{
-    const int MAXN = 1 << 21;
-    const int MAXP = 22;
-    const char * S;
-    int N, gap;
-    int sa[MAXN], pos[MAXN], tmp[MAXN], lcp[MAXN];
-    int P[MAXP][MAXN];
-    int height[MAXP][MAXN];
-    int NP;
+class SuffixArray {
+ public:
+  SuffixArray(const char* s) : n_(strlen(s)), s_(s) {
+    assert(n_ > 0);
+    alloc();
+  }
 
-    bool sufCmp(int i, int j)
-    {
+  ~SuffixArray() {
+    dealloc();
+  }
+
+ private:
+  int n_;
+  int kmax_;
+  const char *s_;
+  int *sa_, *pos_, *lcp_;
+  int **p_, **height_;
+
+  void alloc() {
+    sa_ = new int[n_]{};
+    pos_ = new int[n_]{};
+    lcp_ = new int[n_]{};
+
+    kmax_ = 32 - __builtin_clz(n_-1);
+    p_ = new int*[kmax_+1]{};
+    height_ = new int*[kmax_+1]{};
+    for (int i = 0; i <= kmax_; i++) {
+      p_[i] = new int[n_]{};
+      height_[i] = new int[n_]{};
+    }
+  }
+
+  void dealloc() {
+    delete[] sa_;
+    delete[] pos_;
+    delete[] lcp_;
+
+    for (int i = 0; i <= kmax_; i++) {
+      delete[] p_[i];
+      delete[] height_[i];
+    }
+    delete[] p_;
+    delete[] height_;
+  }
+
+  void buildSA() {
+    int gap = 0;
+    auto sufCmp = [pos = pos_, &gap, n = n_] (int i, int j) {
         if (pos[i] != pos[j])
-            return pos[i] < pos[j];
+          return pos[i] < pos[j];
         i += gap;
         j += gap;
-        return (i < N && j < N) ? pos[i] < pos[j] : i > j;
+        return (i < n && j < n) ? pos[i] < pos[j] : i > j;
+      };
+    int tmp[n_] = {0};
+    REP(i, n_) sa_[i] = i, pos_[i] = s_[i];
+    sort(sa_, sa_ + n_, sufCmp);
+    REP(i, n_-1) tmp[i+1] = tmp[i] + sufCmp(sa_[i], sa_[i+1]);
+    REP(i, n_) p_[0][sa_[i]] = tmp[i];
+    for (int k = 1; k <= kmax_; k++) {
+      gap = 1 << (k-1);
+      sort(sa_, sa_ + n_, sufCmp);
+      REP(i, n_ - 1) tmp[i + 1] = tmp[i] + sufCmp(sa_[i], sa_[i + 1]);
+      REP(i, n_) pos_[sa_[i]] = tmp[i];
+      REP(i, n_) p_[k][sa_[i]] = tmp[i];
     }
+  }
 
-    void buildSA()
-    {
-        N = strlen(S);
-        REP(i, N) sa[i] = i, pos[i] = S[i];
-        gap = 0;
-        sort(sa, sa + N, sufCmp);
-        REP(i, N-1) tmp[i+1] = tmp[i] + sufCmp(sa[i], sa[i+1]);
-        REP(i, N) P[0][sa[i]] = tmp[i];
-        for (NP = 1, gap = 1; gap >> 1 < N; gap *= 2)
-        {
-            sort(sa, sa + N, sufCmp);
-            REP(i, N - 1) tmp[i + 1] = tmp[i] + sufCmp(sa[i], sa[i + 1]);
-            REP(i, N) pos[sa[i]] = tmp[i];
-            REP(i, N) P[NP][sa[i]] = tmp[i];
-            NP++;
-            //if (tmp[N - 1] == N - 1) break;
-        }
+  void buildLCP() {
+    for (int i = 0, k = 0; i < n_; ++i) if (pos_[i] != n_ - 1) {
+      for (int j = sa_[pos_[i] + 1]; s_[i + k] == s_[j + k];) {
+        ++k;
+      }
+      lcp_[pos_[i]] = k;
+      if (k) {
+        --k;
+      }
     }
+  }
 
-    void buildLCP()
-    {
-        for (int i = 0, k = 0; i < N; ++i) if (pos[i] != N - 1)
-        {
-            for (int j = sa[pos[i] + 1]; S[i + k] == S[j + k];)
-                ++k;
-            lcp[pos[i]] = k;
-            if (k)--k;
-        }
+  void buildRMQ() {
+    for (int i = 0; i < n_; i++) {
+      height_[0][i] = lcp_[i];
     }
-
-    void buildRMQ() {
-        for (int i = 0; i < N; i++) {
-            height[0][i] = lcp[i];
-        }
-        for (int j = 1; (1<<j) < N; j++) {
-            for (int i = 0; i < N; i++) {
-                if (i + (1<<j) > N) break;
-                height[j][i] = min(height[j-1][i], height[j-1][i+(1<<(j-1))]);
-            }
-        }
+    for (int j = 1; (1<<j) < n_; j++) {
+      for (int i = 0; i < n_; i++) {
+        if (i + (1<<j) > n_) break;
+        height_[j][i] = min(height_[j-1][i], height_[j-1][i+(1<<(j-1))]);
+      }
     }
+  }
 
-    int calcLCP(int x, int y) {
-        x = pos[x]; y = pos[y];
-        if (x > y) swap(x, y);
-        int k = 0;
-        while ((1<<(k+1)) < (y-x)) k++;
-        return min(height[k][x], height[k][y-(1<<k)]);
-    }
-} // end namespace SuffixArray
+  int calcLCP(int x, int y) const {
+    x = pos_[x]; y = pos_[y];
+    if (x > y) swap(x, y);
+    int k = 0;
+    while ((1<<(k+1)) < (y-x)) k++;
+    return min(height_[k][x], height_[k][y-(1<<k)]);
+  }
 
-/* usage example
-    SuffixArray::S = s.c_str();
-    SuffixArray::buildSA();
-    SuffixArray::buildLCP();
-*/
+  friend class SuffixArrayTest;
+};
--- cpp/test/suffix_array_test.cpp
+++ cpp/test/suffix_array_test.cpp
@@ -6,81 +6,78 @@ using namespace std;
 #include <cppunit/extensions/HelperMacros.h>
 
 class SuffixArrayTest:public CppUnit::TestFixture {
-public:
-    void setUp() {}
-    void tearDown() {}
+ public:
+  void setUp() {}
+  void tearDown() {}
 
-    void testOneCharString() {
-        string s = "aaa";
+  void testOneCharString() {
+    string s = "aaa";
 
-        SuffixArray::S = s.c_str();
-        SuffixArray::buildSA();
-        SuffixArray::buildLCP();
+    SuffixArray sa(s.c_str());
+    sa.buildSA();
+    sa.buildLCP();
 
-        CPPUNIT_ASSERT_EQUAL(SuffixArray::sa[0], 2);
-        CPPUNIT_ASSERT_EQUAL(SuffixArray::sa[1], 1);
-        CPPUNIT_ASSERT_EQUAL(SuffixArray::sa[2], 0);
+    CPPUNIT_ASSERT_EQUAL(sa.sa_[0], 2);
+    CPPUNIT_ASSERT_EQUAL(sa.sa_[1], 1);
+    CPPUNIT_ASSERT_EQUAL(sa.sa_[2], 0);
 
-        CPPUNIT_ASSERT_EQUAL(SuffixArray::pos[0], 2);
-        CPPUNIT_ASSERT_EQUAL(SuffixArray::pos[1], 1);
-        CPPUNIT_ASSERT_EQUAL(SuffixArray::pos[2], 0);
+    CPPUNIT_ASSERT_EQUAL(sa.pos_[0], 2);
+    CPPUNIT_ASSERT_EQUAL(sa.pos_[1], 1);
+    CPPUNIT_ASSERT_EQUAL(sa.pos_[2], 0);
 
-        CPPUNIT_ASSERT_EQUAL(SuffixArray::lcp[0], 1);
-        CPPUNIT_ASSERT_EQUAL(SuffixArray::lcp[1], 2);
-        CPPUNIT_ASSERT_EQUAL(SuffixArray::lcp[2], 0);
+    CPPUNIT_ASSERT_EQUAL(sa.lcp_[0], 1);
+    CPPUNIT_ASSERT_EQUAL(sa.lcp_[1], 2);
+    CPPUNIT_ASSERT_EQUAL(sa.lcp_[2], 0);
+  }
+
+  void testShortString() {
+    string s = "aba";
+
+    SuffixArray sa(s.c_str());
+    sa.buildSA();
+    sa.buildLCP();
+    CPPUNIT_ASSERT_EQUAL(sa.sa_[0], 2);
+    CPPUNIT_ASSERT_EQUAL(sa.sa_[1], 0);
+    CPPUNIT_ASSERT_EQUAL(sa.sa_[2], 1);
+
+    CPPUNIT_ASSERT_EQUAL(sa.pos_[0], 1);
+    CPPUNIT_ASSERT_EQUAL(sa.pos_[1], 2);
+    CPPUNIT_ASSERT_EQUAL(sa.pos_[2], 0);
+
+    CPPUNIT_ASSERT_EQUAL(sa.lcp_[0], 1);
+    CPPUNIT_ASSERT_EQUAL(sa.lcp_[1], 0);
+    CPPUNIT_ASSERT_EQUAL(sa.lcp_[2], 0);
+  }
+
+  void testPMatrix() {
+    string s = "bobocel";
+    SuffixArray sa(s.c_str());
+    sa.buildSA();
+    sa.buildLCP();
+
+    int golden[4][7] = {
+      {0, 4, 0, 4, 1, 2, 3},
+      {0, 4, 0, 5, 1, 2, 3},
+      {0, 5, 1, 6, 2, 3, 4},
+      {0, 5, 1, 6, 2, 3, 4},
+    };
+    for (int i = 0; i < 4; i++) {
+      for (int j = 0; j < 7; j++) {
+        CPPUNIT_ASSERT_EQUAL(golden[i][j], sa.p_[i][j]);
+      }
     }
 
-    void testShortString() {
-        string s = "aba";
+    sa.buildRMQ();
+    CPPUNIT_ASSERT_EQUAL(2, sa.calcLCP(2, 0));
+    CPPUNIT_ASSERT_EQUAL(1, sa.calcLCP(1, 3));
+    CPPUNIT_ASSERT_EQUAL(0, sa.calcLCP(0, 6));
+  }
 
-        SuffixArray::S = s.c_str();
-        SuffixArray::buildSA();
-        SuffixArray::buildLCP();
-
-        CPPUNIT_ASSERT_EQUAL(SuffixArray::sa[0], 2);
-        CPPUNIT_ASSERT_EQUAL(SuffixArray::sa[1], 0);
-        CPPUNIT_ASSERT_EQUAL(SuffixArray::sa[2], 1);
-
-        CPPUNIT_ASSERT_EQUAL(SuffixArray::pos[0], 1);
-        CPPUNIT_ASSERT_EQUAL(SuffixArray::pos[1], 2);
-        CPPUNIT_ASSERT_EQUAL(SuffixArray::pos[2], 0);
-
-        CPPUNIT_ASSERT_EQUAL(SuffixArray::lcp[0], 1);
-        CPPUNIT_ASSERT_EQUAL(SuffixArray::lcp[1], 0);
-        CPPUNIT_ASSERT_EQUAL(SuffixArray::lcp[2], 0);
-    }
-
-    void testPMatrix() {
-        string s = "bobocel";
-
-        SuffixArray::S = s.c_str();
-        SuffixArray::buildSA();
-        SuffixArray::buildLCP();
-
-        int golden[5][7] = {
-            {0, 4, 0, 4, 1, 2, 3},
-            {0, 4, 0, 5, 1, 2, 3},
-            {0, 5, 1, 6, 2, 3, 4},
-            {0, 5, 1, 6, 2, 3, 4},
-            {0, 5, 1, 6, 2, 3, 4},
-        };
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < 7; j++) {
-                CPPUNIT_ASSERT_EQUAL(golden[i][j], SuffixArray::P[i][j]);
-            }
-        }
-        
-        SuffixArray::buildRMQ();
-        CPPUNIT_ASSERT_EQUAL(2, SuffixArray::calcLCP(2, 0));
-        CPPUNIT_ASSERT_EQUAL(1, SuffixArray::calcLCP(1, 3));
-        CPPUNIT_ASSERT_EQUAL(0, SuffixArray::calcLCP(0, 6));
-    }
-
-    CPPUNIT_TEST_SUITE(SuffixArrayTest);
-    CPPUNIT_TEST(testOneCharString);
-    CPPUNIT_TEST(testShortString);
-    CPPUNIT_TEST(testPMatrix);
-    CPPUNIT_TEST_SUITE_END();
+  CPPUNIT_TEST_SUITE(SuffixArrayTest);
+  CPPUNIT_TEST(testOneCharString);
+  CPPUNIT_TEST(testShortString);
+  CPPUNIT_TEST(testPMatrix);
+  CPPUNIT_TEST_SUITE_END();
 };
 
 CPPUNIT_TEST_SUITE_REGISTRATION(SuffixArrayTest);
