f9bb52e64be67e0658556b5e0099d1e7d1dcc5d6
atubo <err722@yahoo.com>
2021-05-29 10:14:34 -0700

    [Hungarian] re-indent

--- cpp/src/graph/hungarian.h
+++ cpp/src/graph/hungarian.h
@@ -1,88 +1,88 @@
 // Hungarian method to find maximal match in bipartite graph
 // using DFS, time complexity O(N^3)
 class Hungarian {
-private:
-    int nx_, ny_;
-    bool** g_;
+ private:
+  int nx_, ny_;
+  bool** g_;
 
-    int *cx_, *cy_;
-    bool *mk_;
+  int *cx_, *cy_;
+  bool *mk_;
 
-    void alloc() {
-        g_ = new bool*[nx_];
-        for (int i = 0; i < nx_; i++) {
-            g_[i] = new bool[ny_];
+  void alloc() {
+    g_ = new bool*[nx_];
+    for (int i = 0; i < nx_; i++) {
+      g_[i] = new bool[ny_];
+    }
+    cx_ = new int[nx_];
+    cy_ = new int[ny_];
+    mk_ = new bool[ny_];
+  }
+
+  void dealloc() {
+    for (int i = 0; i < nx_; i++) {
+      delete[] g_[i];
+    }
+    delete[] g_;
+
+    delete[] cx_;
+    delete[] cy_;
+    delete[] mk_;
+  }
+
+  int path(int u) {
+    for (int v = 0; v < ny_; v++) {
+      if (g_[u][v] && !mk_[v]) {
+        mk_[v] = true;
+        if (cy_[v] == -1 || path(cy_[v])) {
+          cx_[u] = v;
+          cy_[v] = u;
+          return 1;
         }
-        cx_ = new int[nx_];
-        cy_ = new int[ny_];
-        mk_ = new bool[ny_];
+      }
     }
+    return 0;
+  }
 
-    void dealloc() {
-        for (int i = 0; i < nx_; i++) {
-            delete[] g_[i];
-        }
-        delete[] g_;
+ public:
+  Hungarian(int nx, int ny): nx_(nx), ny_(ny) {
+    alloc();
+    reset();
+  }
 
-        delete[] cx_;
-        delete[] cy_;
-        delete[] mk_;
+  ~Hungarian() {
+    dealloc();
+  }
+
+  void reset() {
+    for (int i = 0; i < nx_; i++) {
+      memset(g_[i], 0, ny_ * sizeof(bool));
     }
+  }
 
-    int path(int u) {
-        for (int v = 0; v < ny_; v++) {
-            if (g_[u][v] && !mk_[v]) {
-                mk_[v] = true;
-                if (cy_[v] == -1 || path(cy_[v])) {
-                    cx_[u] = v;
-                    cy_[v] = u;
-                    return 1;
-                }
-            }
-        }
-        return 0;
+  void addEdge(int x, int y) {
+    g_[x][y] = true;
+  }
+
+  int maxMatch() {
+    int res = 0;
+    memset(cx_, -1, nx_ * sizeof(int));
+    memset(cy_, -1, ny_ * sizeof(int));
+    for (int i = 0; i < nx_; i++) {
+      if (cx_[i] == -1) {
+        memset(mk_, 0, ny_ * sizeof(bool));
+        res += path(i);
+      }
     }
+    return res;
+  }
 
-public:
-    Hungarian(int nx, int ny): nx_(nx), ny_(ny) {
-        alloc();
-        reset();
-    }
+  struct Match {
+    int nx, ny;
+    const int *cx, *cy;
+    bool const * const *g;
+  };
 
-    ~Hungarian() {
-        dealloc();
-    }
-
-    void reset() {
-        for (int i = 0; i < nx_; i++) {
-            memset(g_[i], 0, ny_ * sizeof(bool));
-        }
-    }
-
-    void addEdge(int x, int y) {
-        g_[x][y] = true;
-    }
-
-    int maxMatch() {
-        int res = 0;
-        memset(cx_, -1, nx_ * sizeof(int));
-        memset(cy_, -1, ny_ * sizeof(int));
-        for (int i = 0; i < nx_; i++) {
-            if (cx_[i] == -1) {
-                memset(mk_, 0, ny_ * sizeof(bool));
-                res += path(i);
-            }
-        }
-        return res;
-    }
-
-    struct Match {
-        int nx, ny;
-        const int *cx, *cy;
-        bool const * const *g;
-    };
-
-    Match getMatch() const {
-        return {nx_, ny_, cx_, cy_, g_};
-    }
+  Match getMatch() const {
+    return {nx_, ny_, cx_, cy_, g_};
+  }
 };
