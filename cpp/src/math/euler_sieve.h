3b2e5f6a6994f8b134d43d04802e432b67ed931c
atubo <err722@yahoo.com>
2021-05-31 09:14:45 -0700

    [Euler sieve] re-indent

--- cpp/src/math/euler_sieve.h
+++ cpp/src/math/euler_sieve.h
@@ -1,51 +1,51 @@
 
 class EulerSieve {
-private:
-public:
-    vector<bool> isPrime;
-    vector<int> primes, mu, phi, sigma;
+ private:
+ public:
+  vector<bool> isPrime;
+  vector<int> primes, mu, phi, sigma;
 
-    EulerSieve(int N) {
-        assert(N > 1);
+  EulerSieve(int N) {
+    assert(N > 1);
 
-        isPrime.resize(N+1, true);
-        mu.resize(N+1);
-        phi.resize(N+1);
-        sigma.resize(N+1);
+    isPrime.resize(N+1, true);
+    mu.resize(N+1);
+    phi.resize(N+1);
+    sigma.resize(N+1);
 
-        // local variable to calculate sigma (divisor function)
-        // i = pj^k * ..., and pi(i) = (1 + pj + pj^2 + ...)
-        vector<int> pi(N+1);
+    // local variable to calculate sigma (divisor function)
+    // i = pj^k * ..., and pi(i) = (1 + pj + pj^2 + ...)
+    vector<int> pi(N+1);
 
-        isPrime[0] = isPrime[1] = false;
-        mu[1] = 1;
-        phi[1] = 1;
-        sigma[1] = 1;
-        for (int i = 2; i <= N; i++) {
-            if (isPrime[i]) {
-                primes.push_back(i);
-                mu[i] = -1;
-                phi[i] = i - 1;
-                sigma[i] = pi[i] = i + 1;
-            }
+    isPrime[0] = isPrime[1] = false;
+    mu[1] = 1;
+    phi[1] = 1;
+    sigma[1] = 1;
+    for (int i = 2; i <= N; i++) {
+      if (isPrime[i]) {
+        primes.push_back(i);
+        mu[i] = -1;
+        phi[i] = i - 1;
+        sigma[i] = pi[i] = i + 1;
+      }
 
-            for (int j = 0; j < (int)primes.size(); j++) {
-                int64_t t = (int64_t)i * primes[j];
-                if ( t > N) break;
-                isPrime[t] = false;
-                if (i % primes[j] == 0) {
-                    mu[t] = 0;
-                    phi[t] = phi[i] * primes[j];
-                    pi[t] = pi[i] * primes[j] + 1;
-                    sigma[t] = sigma[i]/pi[i] * pi[t];
-                    break;
-                } else {
-                    mu[t] = -mu[i];
-                    phi[t] = phi[i] * (primes[j] - 1);
-                    pi[t] = primes[j] + 1;
-                    sigma[t] = sigma[primes[j]] * sigma[i];
-                }
-            }
+      for (int j = 0; j < (int)primes.size(); j++) {
+        int64_t t = (int64_t)i * primes[j];
+        if ( t > N) break;
+        isPrime[t] = false;
+        if (i % primes[j] == 0) {
+          mu[t] = 0;
+          phi[t] = phi[i] * primes[j];
+          pi[t] = pi[i] * primes[j] + 1;
+          sigma[t] = sigma[i]/pi[i] * pi[t];
+          break;
+        } else {
+          mu[t] = -mu[i];
+          phi[t] = phi[i] * (primes[j] - 1);
+          pi[t] = primes[j] + 1;
+          sigma[t] = sigma[primes[j]] * sigma[i];
         }
+      }
     }
+  }
 };
