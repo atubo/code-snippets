561351c09139f87f207587bf162d414cbbeed5f1
atubo <err722@yahoo.com>
2021-06-26 12:48:40 -0700

    [UnionFind] re-indent

--- cpp/src/data_structures/UnionFind.h
+++ cpp/src/data_structures/UnionFind.h
@@ -1,61 +1,61 @@
 
 class UnionFind {
-private:
-    const int N;
-    vector<int> rank;
-    vector<int> parent;
-    vector<int> size;
+ private:
+  const int N;
+  vector<int> rank;
+  vector<int> parent;
+  vector<int> size;
 
-    void makeSet(int k) {
-        assert(0 <= k && k < N);
+  void makeSet(int k) {
+    assert(0 <= k && k < N);
 
-        parent[k] = k;
-        rank[k] = 0;
-        size[k] = 1;
+    parent[k] = k;
+    rank[k] = 0;
+    size[k] = 1;
+  }
+
+  int link(int u, int v) {
+    if (u == v) return u;
+    if (rank[u] > rank[v]) {
+      parent[v] = u;
+      size[u] += size[v];
+      return u;
+    } else {
+      parent[u] = v;
+      size[v] += size[u];
+      if (rank[u] == rank[v]) {
+        rank[v]++;
+      }
+      return v;
     }
+  }
 
-    int link(int u, int v) {
-        if (u == v) return u;
-        if (rank[u] > rank[v]) {
-            parent[v] = u;
-            size[u] += size[v];
-            return u;
-        } else {
-            parent[u] = v;
-            size[v] += size[u];
-            if (rank[u] == rank[v]) {
-                rank[v]++;
-            }
-            return v;
-        }
+ public:
+  UnionFind(int n): N(n) {
+    rank.resize(N);
+    parent.resize(N);
+    size.resize(N);
+
+    reset();
+  }
+
+  void reset() {
+    for (int i = 0; i < N; i++) {
+      makeSet(i);
     }
+  }
 
-public:
-    UnionFind(int n): N(n) {
-        rank.resize(N);
-        parent.resize(N);
-        size.resize(N);
+  int find(int k) {
+    if (parent[k] == k) return k;
+    int root = find(parent[k]);
+    return parent[k] = root;
+  }
 
-        reset();
-    }
+  int join(int u, int v) {
+    return link(find(u), find(v));
+  }
 
-    void reset() {
-        for (int i = 0; i < N; i++) {
-            makeSet(i);
-        }
-    }
-
-    int find(int k) {
-        if (parent[k] == k) return k;
-        int root = find(parent[k]);
-        return parent[k] = root;
-    }
-
-    int join(int u, int v) {
-        return link(find(u), find(v));
-    }
-
-    int count(int u) {
-        return size[find(u)];
-    }
+  int count(int u) {
+    return size[find(u)];
+  }
 };
--- cpp/test/UnionFindTest.cpp
+++ cpp/test/UnionFindTest.cpp
@@ -7,59 +7,59 @@ using namespace std;
 #include <cppunit/extensions/HelperMacros.h>
 
 class UnionFindTest:public CppUnit::TestFixture {
-public:
-    void setUp() {}
-    void tearDown() {}
+ public:
+  void setUp() {}
+  void tearDown() {}
 
-    void testUnionFind() {
-        UnionFind uf(3);
-        for (int i = 0; i < 3; i++) {
-            CPPUNIT_ASSERT(uf.find(i) == i);
-        }
-
-        uf.join(1, 2);
-        CPPUNIT_ASSERT(uf.find(1) == uf.find(2));
-        CPPUNIT_ASSERT(uf.find(0) != uf.find(1));
+  void testUnionFind() {
+    UnionFind uf(3);
+    for (int i = 0; i < 3; i++) {
+      CPPUNIT_ASSERT(uf.find(i) == i);
     }
 
-    void testSizeCount() {
-        UnionFind uf(5);
-        for (int i = 0; i < 5; i++) {
-            CPPUNIT_ASSERT(uf.count(i) == 1);
-        }
-        uf.join(0, 1);
-        CPPUNIT_ASSERT(uf.count(0) == 2);
-        CPPUNIT_ASSERT(uf.count(1) == 2);
-        CPPUNIT_ASSERT(uf.count(2) == 1);
-        CPPUNIT_ASSERT(uf.count(3) == 1);
-        CPPUNIT_ASSERT(uf.count(4) == 1);
+    uf.join(1, 2);
+    CPPUNIT_ASSERT(uf.find(1) == uf.find(2));
+    CPPUNIT_ASSERT(uf.find(0) != uf.find(1));
+  }
 
-        uf.join(3, 4);
-        CPPUNIT_ASSERT(uf.count(0) == 2);
-        CPPUNIT_ASSERT(uf.count(1) == 2);
-        CPPUNIT_ASSERT(uf.count(2) == 1);
-        CPPUNIT_ASSERT(uf.count(3) == 2);
-        CPPUNIT_ASSERT(uf.count(4) == 2);
-
-        uf.join(1, 2);
-        CPPUNIT_ASSERT(uf.count(0) == 3);
-        CPPUNIT_ASSERT(uf.count(1) == 3);
-        CPPUNIT_ASSERT(uf.count(2) == 3);
-        CPPUNIT_ASSERT(uf.count(3) == 2);
-        CPPUNIT_ASSERT(uf.count(4) == 2);
-
-        uf.join(0, 4);
-        CPPUNIT_ASSERT(uf.count(0) == 5);
-        CPPUNIT_ASSERT(uf.count(1) == 5);
-        CPPUNIT_ASSERT(uf.count(2) == 5);
-        CPPUNIT_ASSERT(uf.count(3) == 5);
-        CPPUNIT_ASSERT(uf.count(4) == 5);
+  void testSizeCount() {
+    UnionFind uf(5);
+    for (int i = 0; i < 5; i++) {
+      CPPUNIT_ASSERT(uf.count(i) == 1);
     }
+    uf.join(0, 1);
+    CPPUNIT_ASSERT(uf.count(0) == 2);
+    CPPUNIT_ASSERT(uf.count(1) == 2);
+    CPPUNIT_ASSERT(uf.count(2) == 1);
+    CPPUNIT_ASSERT(uf.count(3) == 1);
+    CPPUNIT_ASSERT(uf.count(4) == 1);
 
-    CPPUNIT_TEST_SUITE(UnionFindTest);
-    CPPUNIT_TEST(testUnionFind);
-    CPPUNIT_TEST(testSizeCount);
-    CPPUNIT_TEST_SUITE_END();
+    uf.join(3, 4);
+    CPPUNIT_ASSERT(uf.count(0) == 2);
+    CPPUNIT_ASSERT(uf.count(1) == 2);
+    CPPUNIT_ASSERT(uf.count(2) == 1);
+    CPPUNIT_ASSERT(uf.count(3) == 2);
+    CPPUNIT_ASSERT(uf.count(4) == 2);
+
+    uf.join(1, 2);
+    CPPUNIT_ASSERT(uf.count(0) == 3);
+    CPPUNIT_ASSERT(uf.count(1) == 3);
+    CPPUNIT_ASSERT(uf.count(2) == 3);
+    CPPUNIT_ASSERT(uf.count(3) == 2);
+    CPPUNIT_ASSERT(uf.count(4) == 2);
+
+    uf.join(0, 4);
+    CPPUNIT_ASSERT(uf.count(0) == 5);
+    CPPUNIT_ASSERT(uf.count(1) == 5);
+    CPPUNIT_ASSERT(uf.count(2) == 5);
+    CPPUNIT_ASSERT(uf.count(3) == 5);
+    CPPUNIT_ASSERT(uf.count(4) == 5);
+  }
+
+  CPPUNIT_TEST_SUITE(UnionFindTest);
+  CPPUNIT_TEST(testUnionFind);
+  CPPUNIT_TEST(testSizeCount);
+  CPPUNIT_TEST_SUITE_END();
 };
 
 CPPUNIT_TEST_SUITE_REGISTRATION(UnionFindTest);
